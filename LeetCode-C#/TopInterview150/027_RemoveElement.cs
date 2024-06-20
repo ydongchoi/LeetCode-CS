e8e259f240d78f41e06aa65793dfe3bcff4dc2c7
ydongchoi <105weapon@gmail.com>
2024-06-20 12:56:35 -0700

    [feature] Added 027_RemoveElements Solution.

---
+++ LeetCode-C#/TopInterview150/027_RemoveElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_C_.TopInterview150
+{
+    public class _027_RemoveElement
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int answer = 0;
+            List<int> data = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] != val)
+                {
+                    data.Add(nums[i]);
+                    answer++;
+                }
+            }
+
+            for(int i = 0; i < data.Count; ++i)
+            {
+                nums[i] = data[i];
+            }
+
+            return answer;
+        }
+    }
+}
