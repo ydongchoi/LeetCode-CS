5e9f0a7de76c4d4d4ca6a44ba8099c92df9e3a2b
ydongchoi <105weapon@gmail.com>
2024-06-21 12:43:11 -0700

    [feature] Added 169_MajorityElement Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/169_MajorityElement.cs
@@ -0,0 +1,35 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/majority-element/description/?envType=study-plan-v2&envId=top-interview-150
+
+using System.Security.Cryptography;
+
+namespace LeetCode_C_.TopInterview150
+{
+    public class _169_MajorityElement
+    {
+        public int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int answer = 0; int maxCnt = 0;
+
+            for(int idx = 0; idx < nums.Length; idx++)
+            {
+                if (!dic.ContainsKey(nums[idx]))
+                {
+                    dic.Add(nums[idx], 1);
+                    maxCnt = maxCnt > 1 ? maxCnt : 1;
+                    answer = maxCnt > 1 ? answer : nums[idx];
+                }
+                else
+                {
+                    dic[nums[idx]]++;
+                    maxCnt = maxCnt > dic[nums[idx]] ? maxCnt : dic[nums[idx]];
+                    answer = maxCnt > dic[nums[idx]] ? answer : nums[idx];
+                }
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/TopInterview150/169_MajorityElement_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150
+{
+    public class _169_MajorityElement_Test
+    {
+        private readonly _169_MajorityElement _algorithm;
+
+        public _169_MajorityElement_Test()
+        {
+            _algorithm = new _169_MajorityElement();
+        }
+
+        [Theory]
+        [InlineData(new int[] {3,2,3}, 3)]
+        [InlineData(new int[] {2,2,1,1,1,2,2}, 2)]
+        public void MajorityElement_Input_Returns(int[] nums, int expectedResult)
+        {
+            // Act
+            var result = _algorithm.MajorityElement(nums);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+    }
+}
