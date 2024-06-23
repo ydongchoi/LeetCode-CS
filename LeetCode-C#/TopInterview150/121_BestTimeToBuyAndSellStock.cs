99f77f1017c0d3fd7cb7f30093d37c639f02e591
ydongchoi <105weapon@gmail.com>
2024-06-23 14:38:12 -0700

    [feature] Added 121_BestTimeToBuyAndSellStock Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/121_BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,36 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+namespace LeetCode_C_.TopInterview150
+{
+    public class _121_BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int buy, sell;
+            buy = sell = prices[0];
+            int profit = 0;
+
+            for (int idx = 0; idx < prices.Length; ++idx)
+            {
+
+                if (buy > prices[idx])
+                {
+                    buy = prices[idx];
+                    sell = 0;
+                }
+
+                if (sell < prices[idx])
+                {
+                    sell = prices[idx];
+                }
+
+                int tmp = sell - buy;
+                profit = profit > tmp ? profit : tmp;
+            }
+
+            return profit;
+        }
+    }
+}
---
+++ Tests/TopInterview150/121_BestTimeToBuyAndSellStockTest.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150
+{
+    public class _121_BestTimeToBuyAndSellStockTest
+    {
+        private readonly _121_BestTimeToBuyAndSellStock _algorithm;
+
+        public _121_BestTimeToBuyAndSellStockTest()
+        {
+            _algorithm = new _121_BestTimeToBuyAndSellStock();
+        }
+
+        [Theory]
+        [InlineData(new int[] {7,1,5,3,6,4}, 5)]
+        [InlineData(new int[] {7,6,4,3,1}, 0)]
+        public void MaxProfit_Input_Returns(int[] prices, int expectedResult)
+        {
+            // Act
+            var result = _algorithm.MaxProfit(prices);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+    }
+}
