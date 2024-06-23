8cbf5b2ee0451962420d2eb6e301b8b8881ef7b4
ydongchoi <105weapon@gmail.com>
2024-06-23 14:46:29 -0700

    [feature] Added 643_MaximumAverageSubarrayI Solution and Unit tests.

---
+++ LeetCode-C#/TopInterview150/643_MaximumAverageSubarrayI.cs
@@ -0,0 +1,31 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/maximum-average-subarray-i/description/
+
+namespace LeetCode_C_.TopInterview150
+{
+    public class _643_MaximumAverageSubarrayI
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            double answer = 0;
+            double tmpSum = 0;
+
+            for (int idx = 0; idx < k; ++idx)
+            {
+                tmpSum += nums[idx];
+            }
+            answer = tmpSum;
+
+            for (int idx = k; idx < nums.Length; ++idx)
+            {
+                tmpSum = tmpSum - nums[idx - k];
+                tmpSum = tmpSum + nums[idx];
+
+                answer = answer > tmpSum ? answer : tmpSum;
+            }
+
+            return answer / k;
+        }
+    }
+}
---
+++ Tests/TopInterview150/643_MaximumAverageSubarrayI_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150
+{
+    public class _643_MaximumAverageSubarrayI_Test
+    {
+        private readonly _643_MaximumAverageSubarrayI _algorithm;
+
+        public _643_MaximumAverageSubarrayI_Test()
+        {
+            _algorithm = new _643_MaximumAverageSubarrayI();
+        }
+
+        [Theory]
+        [InlineData(new int[] {1,12,-5,-6,50,3}, 4, 12.75000)]
+        [InlineData(new int[] {5}, 1, 5.00000)]
+        public void FindMaxAverage(int[] nums, int k, double expectedResult)
+        {
+            // Act
+            var result = _algorithm.FindMaxAverage(nums, k);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+
+        }
+    }
+}
