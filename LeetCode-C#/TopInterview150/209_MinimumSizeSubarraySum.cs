b22c564c6062465682d5edd554f36b7b357f2971
ydongchoi <105weapon@gmail.com>
2024-06-23 14:57:57 -0700

    [feature] Added 209_MininumSizeSubarraySum Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/209_MinimumSizeSubarraySum.cs
@@ -0,0 +1,30 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/minimum-size-subarray-sum/description/https://leetcode.com/problems/minimum-size-subarray-sum/description/
+
+namespace LeetCode_C_.TopInterview150
+{
+    public class _209_MinimumSizeSubarraySum
+    {
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            int l = 0;
+            int answer = int.MaxValue;
+            int sum = 0;
+
+            for (int idx = 0; idx < nums.Length; idx++)
+            {
+                sum += nums[idx];
+
+                while (sum >= target)
+                {
+                    answer = Math.Min(answer, idx - l + 1);
+                    sum -= nums[l];
+                    l++;
+                }
+            }
+
+            return answer != int.MaxValue ? answer : 0;
+        }
+    }
+}
---
+++ Tests/TopInterview150/209_MinimumSizeSubarraySum_Test.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode_C_.TopInterview150;
+using NuGet.Frameworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150
+{
+    public class _209_MinimumSizeSubarraySum_Test
+    {
+        private readonly _209_MinimumSizeSubarraySum _algorithm;
+
+        public _209_MinimumSizeSubarraySum_Test()
+        {
+            _algorithm = new _209_MinimumSizeSubarraySum();
+        }
+
+        [Theory]
+        [InlineData(7, new int[] {2,3,1,2,4,3}, 2)]
+        [InlineData(4, new int[] {1,4,4}, 1)]
+        [InlineData(11, new int[] {1,1,1,1,1,1,1,1}, 0)]
+        public void MinSubArrayLen_Input_Returns(int target, int[] nums, int expectedResult)
+        {
+            // Act
+            var result = _algorithm.MinSubArrayLen(target, nums);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
