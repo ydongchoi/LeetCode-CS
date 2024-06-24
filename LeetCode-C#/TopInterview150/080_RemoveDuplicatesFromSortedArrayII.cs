54828474eb103ee46874ac2fdd8f79042aaf7f37
ydongchoi <105weapon@gmail.com>
2024-06-24 16:54:35 -0700

    [feature] Added 080_RemoveDuplicatesFromSortedArrayII Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/080_RemoveDuplicatesFromSortedArrayII.cs
@@ -0,0 +1,35 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150
+{
+    public class _080_RemoveDuplicatesFromSortedArrayII
+    {
+		public int RemoveDuplicates(int[] nums)
+		{
+			int k = 1;
+            int numCnt = 1;
+
+            for (int idx =  1; idx < nums.Length; ++idx)
+			{
+				if(numCnt >= 2 && nums[idx - 1] == nums[idx])
+				{
+					numCnt++;
+				}
+				else if (nums[idx - 1] == nums[idx])
+				{
+					nums[k++] = nums[idx];
+                    numCnt++;
+				}
+				else
+				{
+                    nums[k++] = nums[idx];
+                    numCnt = 1;
+                }
+            }
+
+			return k;
+		}
+	}
+}
---
+++ Tests/TopInterview150/080_RemoveDuplicatesFromSortedArrayII_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150
+{
+    public class _080_RemoveDuplicatesFromSortedArrayII_Test
+    {
+        private readonly _080_RemoveDuplicatesFromSortedArrayII _algorithm;
+
+        public _080_RemoveDuplicatesFromSortedArrayII_Test()
+        {
+            _algorithm = new _080_RemoveDuplicatesFromSortedArrayII();
+        }
+
+        [Theory]
+        [InlineData(new int[] {1,1,1,2,2,3}, 5, new int[]{1,1,2,2,3})]
+        [InlineData(new int[] {0,0,1,1,1,1,2,3,3,}, 7, new int[]{0,0,1,1,2,3,3})]
+        public void RemoveDuplicates_Input_Returns(int[] nums, int k, int[] expectedNums)
+        {
+            // Act
+            var result = _algorithm.RemoveDuplicates(nums);
+
+            // Assert
+            Assert.Equal(k, result);
+            Assert.Equal(expectedNums[0..k], nums[0..k]);
+        }
+    }
+}
