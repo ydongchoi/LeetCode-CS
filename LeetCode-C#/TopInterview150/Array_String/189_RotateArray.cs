89d5818420f070ab45af90399609bd2ff0bae5e7
ydongchoi <105weapon@gmail.com>
2024-06-24 17:28:42 -0700

    [feature] Created 189_RotateArray Soltuion and Unit test.

---
+++ LeetCode-C#/TopInterview150/Array_String/189_RotateArray.cs
@@ -0,0 +1,22 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/rotate-array/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _189_RotateArray
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            int len = nums.Length;
+            k = k % len;
+            int[] copiedNums = new int[nums.Length];
+
+            for (int idx = 0; idx < nums.Length; idx++)
+                copiedNums[idx] = nums[idx];
+
+            for (int idx = 0; idx < nums.Length; idx++)
+                nums[(idx + k) % len] = copiedNums[idx];
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/189_RotateArray_Test.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _189_RotateArray_Test
+    {
+        private readonly _189_RotateArray _algorithm;
+
+        public _189_RotateArray_Test()
+        {
+            _algorithm = new _189_RotateArray();
+        }
+
+        [Theory]
+        [InlineData(new int[] {1,2,3,4,5,6,7}, 3, new int[] {5,6,7,1,2,3,4})]
+        [InlineData(new int[] {-1,-100,3,99}, 2, new int[] {3,99,-1,-100})]
+        public void Rotate_Input_Returns(int[] nums, int k, int[] expectedResult)
+        {
+            // Act
+            _algorithm.Rotate(nums, k);
+
+            // Assert
+            Assert.Equal(expectedResult, nums);
+        }
+    }
+}
