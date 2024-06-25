6484796c898063d4ffb661aec75053944516049a
ydongchoi <105weapon@gmail.com>
2024-06-25 11:27:45 -0700

    [feature] Added 122_BestTimeToBuyAndSellStockII Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/Array_String/122_BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,25 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _122_BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int profit = 0;
+
+            for(int idx = 1; idx < prices.Length; ++idx)
+            {
+                if (prices[idx - 1] < prices[idx])
+                {
+                    int priceGap = prices[idx] - prices[idx - 1];
+                    profit += priceGap;
+                }
+            }
+
+            return profit;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/122_BestTimeToBuyAndSellStock_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _122_BestTimeToBuyAndSellStock_Test
+    {
+        private readonly _122_BestTimeToBuyAndSellStock _algorithm;
+
+        public _122_BestTimeToBuyAndSellStock_Test()
+        {
+            _algorithm = new _122_BestTimeToBuyAndSellStock();
+        }
+
+        [Theory]
+        [InlineData(new int[] {7,1,5,3,6,4}, 7)]
+        [InlineData(new int[] {1,2,3,4,5}, 4)]
+        [InlineData(new int[] {7,6,4,3,1}, 0)]
+        public void MaxProfit_Input_Returns(int[] prices, int expectedResult)
+        {
+            // Act
+            var result = _algorithm.MaxProfit(prices);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
