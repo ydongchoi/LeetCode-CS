28875b16089f27a1adbbd0f35ae3ee47f57079cf
ydongchoi <105weapon@gmail.com>
2024-06-26 12:29:08 -0700

    [feature] Added 055_JumpGame Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/Array_String/055_JumpGame.cs
@@ -0,0 +1,38 @@
+﻿
+// Time Complexity : O(2^N) , Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/jump-game/description/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _055_JumpGame
+    {
+        public bool CanJump(int[] nums)
+        {
+            bool[] isJumped = new bool[nums.Length];
+            for (int idx = 0; idx < nums.Length; ++idx) isJumped[idx] = true;
+
+            var result = dfs(0, nums.Length - 1);
+
+            bool dfs(int pos, int end)
+            {
+                if (pos >= end) return true;
+
+                bool canJump = false;
+                for(int jump = 1; jump <= nums[pos]; ++jump)
+                {
+                    if (isJumped[pos + jump] == false) continue;
+
+                    canJump = dfs(pos + jump, end);
+                    
+                    if (canJump) return true;
+                    else isJumped[pos + jump] = canJump;
+                }
+
+                return canJump;
+            }
+
+
+            return result;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/055_JumpGame_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _055_JumpGame_Test
+    {
+        private readonly _055_JumpGame _algorithm;
+
+        public _055_JumpGame_Test()
+        {
+            _algorithm = new _055_JumpGame();
+        }
+
+        [Theory]
+        [InlineData(new int[] {2,3,1,1,4}, true)]
+        [InlineData(new int[] {3,2,1,0,4}, false)]
+        public void CanJump_Input_Returns(int[] nums, bool expectedResult)
+        {
+            // Act
+            var result = _algorithm.CanJump(nums);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+    }
+}
