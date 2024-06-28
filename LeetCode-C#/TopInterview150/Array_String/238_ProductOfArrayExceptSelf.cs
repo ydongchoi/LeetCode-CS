80c85c692bd811495483790fd5be7954abb2d5b4
ydongchoi <105weapon@gmail.com>
2024-06-28 13:25:46 -0700

    [feature] Added 238_ProductOfArrayExceptSelf Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/Array_String/238_ProductOfArrayExceptSelf.cs
@@ -0,0 +1,41 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _238_ProductOfArrayExceptSelf
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int product = 1;
+            int zeroCnt = 0;
+            int zeroIdx = 0;
+
+            for(int idx = 0; idx < nums.Length; ++idx)
+            {
+                if (nums[idx] == 0)
+                {
+                    zeroCnt++; zeroIdx = idx;
+                    continue;
+                }
+
+                product = product * nums[idx];
+            }
+
+            if(zeroCnt >= 1)
+            {
+                for (int idx = 0; idx < nums.Length; ++idx)
+                    nums[idx] = 0;
+                if(zeroCnt == 1) nums[zeroIdx] = product;
+
+            }
+            else
+            {
+                for (int idx = 0; idx < nums.Length; ++idx) nums[idx] = (product / nums[idx]);
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/238_ProductOfArrayExceptSelf_Test.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _238_ProductOfArrayExceptSelf_Test
+    {
+        private readonly _238_ProductOfArrayExceptSelf _algorithm;
+
+        public _238_ProductOfArrayExceptSelf_Test()
+        {
+            _algorithm = new _238_ProductOfArrayExceptSelf();
+        }
+
+        [Theory]
+        [InlineData(new int[] {1,2,3,4}, new int[] {24,12,8,6})]
+        [InlineData(new int[] {-1,1,0,-3,3}, new int[] {0,0,9,0,0})]
+        public void ProductExceptSelf_Input_Returns(int[] nums, int[] expectedResult)
+        {
+            // Act
+            var result = _algorithm.ProductExceptSelf(nums);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
