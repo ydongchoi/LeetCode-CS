85f28f97f7d0c2f49aa90f93681e562e50c87bc3
ydongchoi <105weapon@gmail.com>
2024-06-30 16:11:28 -0700

    [feature] Added 206_ReverseLinkedList Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/LinkedList/206_ReverseLinkedList.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace LeetCode_C_.TopInterview150.LinkedList
+{
+
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+ 
+    public class _206_ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            Stack<ListNode> data = new Stack<ListNode>();
+
+            while (head is not null)
+            {
+                data.Push(head);
+                head = head.next;
+            }
+
+            ListNode start = data.Count == 0 ? null : data.Pop();
+            ListNode answer = start;
+
+            while (data.Count != 0)
+            {
+                start.next = data.Pop();
+                start = start.next;
+            }
+
+            if (start is not null) start.next = null;
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/TopInterview150/LinkedList/206_ReverseLinkedList_Test.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode_C_.TopInterview150.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace Tests.TopInterview150.LinkedList
+{
+    public class _206_ReverseLinkedList_Test
+    {
+        private readonly _206_ReverseLinkedList _algorithm;
+
+        public _206_ReverseLinkedList_Test()
+        {
+            _algorithm = new _206_ReverseLinkedList();
+        }
+
+        [Theory]
+        [InlineData(new int[] {1,2,3,4,5}, new int[] {5,4,3,2,1})]
+        [InlineData(new int[] {1,2}, new int[] {2,1})]
+        public void ReverseList_Input_Returns(int[] nums, int[] expectedResult)
+        {
+            // Arrange
+            var head = new ListNode(nums[0]);
+            var cur = head;
+            for (int idx = 1; idx < nums.Length; idx++)
+            {
+                cur.next = new ListNode(nums[idx]);
+                cur = cur.next;
+            }
+
+            // Act
+            var result = _algorithm.ReverseList(head);
+
+            // Assert
+            foreach(var expResult in expectedResult)
+            {
+                Assert.Equal(expResult, result.val);
+                result = result.next;
+            }
+        }
+    }
+}
