a1599e0ecaa817fd9cbd157ce13fcea73db09ebe
ydongchoi <105weapon@gmail.com>
2024-07-04 14:53:09 -0700

    [fix] Improved Spacious Complexity O(N) to O(1) in 143_ReorderList Solution.

---
+++ LeetCode-C#/TopInterview150/LinkedList/143_ReorderList.cs
@@ -0,0 +1,65 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/reorder-list/description/
+
+namespace LeetCode_C_.TopInterview150.LinkedList
+{
+    public class _143_ReorderList
+    {
+        public void ReorderList(ListNode head)
+        {
+            if (head.next is null) return;
+
+            // Count Node
+            ListNode cur = head; int nodeCnt = 0;
+            while(cur is not null)
+            {
+                nodeCnt++;
+                cur = cur.next;
+            }
+
+            // Seperate Linked List to two parts
+            int halfCnt = (nodeCnt % 2) == 1 ? nodeCnt / 2 + 1 : nodeCnt / 2;
+            ListNode prev = head;  cur = head.next;
+            for (int idx = 0; idx < halfCnt; idx++)
+            {
+                if (idx == halfCnt - 1)
+                {
+                    prev.next = null;
+                    break;
+                }
+                prev = prev.next;
+                cur = cur.next;
+            }
+
+            // Reverse Second Linked List
+            ListNode secondStart = cur;
+            prev = secondStart;
+            cur = secondStart.next;
+            prev.next = null;
+            while(cur is not null)
+            {
+                ListNode tmp = cur.next;
+                cur.next = prev;
+
+                prev = cur;
+                cur = tmp;
+            }
+
+            // Merge two Linked Lists
+            cur = head;
+            while (prev is not null)
+            {
+                ListNode prevTmp = prev.next;
+                ListNode tmp = cur.next;
+
+                cur.next = prev;
+                prev.next = tmp;
+                prev = prevTmp;
+                cur = tmp;
+            }
+
+           
+        }
+    }
+}
---
+++ Tests/TopInterview150/LinkedList/143_ReorderList_Test.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode_C_.TopInterview150.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.LinkedList
+{
+    public class _143_ReorderList_Test
+    {
+        private readonly _143_ReorderList _algorithm;
+
+        public _143_ReorderList_Test()
+        {
+            _algorithm = new _143_ReorderList();
+        }
+
+        [Theory]
+        [InlineData(new int[] {1,2,3,4}, new int[] {1,4,2,3})]
+        [InlineData(new int[] {1,2,3,4,5}, new int[] {1,5,2,4,3})]
+        [InlineData(new int[] {1}, new int[] {1})]
+        public void ReorderList_Input_Returns(int[] nums, int[] expectedResult)
+        {
+            // Assert
+            ListNode head = new ListNode(nums[0]);
+            ListNode cur = head;
+            for(int idx = 1; idx < nums.Length; idx++)
+            {
+                ListNode addedNode = new ListNode(nums[idx]);
+                cur.next = addedNode; cur = cur.next;
+            }
+
+            // Act
+            _algorithm.ReorderList(head);
+
+            // Assign
+            foreach (var expResult in expectedResult)
+            {
+                Assert.Equal(expResult, head.val);
+                head = head.next;
+            }
+        }
+    }
+}
