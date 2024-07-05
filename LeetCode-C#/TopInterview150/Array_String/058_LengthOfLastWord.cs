4cf76a5b17c45bc3e3bbff9575f38b1c3eef9aa6
ydongchoi <105weapon@gmail.com>
2024-07-05 13:57:02 -0700

    [feature] Added 058_LengthOfLastWord Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/Array_String/058_LengthOfLastWord.cs
@@ -0,0 +1,26 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/length-of-last-word/description/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _058_LengthOfLastWord
+    {
+        public int LengthOfLastWord(string s)
+        {
+            int len = s.Length - 1;
+            int lastWordLen = 0;
+
+            for (int idx = len; idx >= 0; idx--)
+            {
+                if (s[idx] != ' ')
+                {
+                    lastWordLen++;
+                }
+                if (s[idx] == ' ' && lastWordLen != 0) break;
+            }
+
+            return lastWordLen;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/058_LengthOfLastWord_Test.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _058_LengthOfLastWord_Test
+    {
+        private readonly _058_LengthOfLastWord _algorithm;
+
+        public _058_LengthOfLastWord_Test()
+        {
+            _algorithm = new _058_LengthOfLastWord();
+        }
+
+        [Theory]
+        [InlineData("Hello World", 5)]
+        [InlineData("   fly me   to   the moon  ", 4)]
+        [InlineData("luffy is still joyboy", 6)]
+        public void LengthOfLastWord_InputString_Returns(string s, int expectedResult)
+        {
+            // Act
+            var result = _algorithm.LengthOfLastWord(s);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
