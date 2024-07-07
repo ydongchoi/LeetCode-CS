c0cd72ccdd3fa268ed7e9e9fb6412d2075a15fb8
ydongchoi <105weapon@gmail.com>
2024-07-07 14:53:02 -0700

    [feature] Added 104_MaximumDepthOfBinaryTree Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/BinaryTree/104_MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,69 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/maximum-depth-of-binary-tree/description/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150.BinaryTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public static TreeNode GetTreeNode(List<int?> trees)
+        {
+            int idx = 0;
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            TreeNode root = new TreeNode(trees[idx].Value);
+            queue.Enqueue(root);
+
+            while (queue.Count > 0 && idx < trees.Count - 1)
+            {
+                TreeNode node = queue.Dequeue();
+                if(node is null) continue;
+
+                node.left = trees[idx + 1] != null ? new TreeNode(trees[idx + 1].Value) : null;
+                queue.Enqueue(node.left);
+                node.right = trees[idx + 2] != null ? new TreeNode(trees[idx + 2].Value) : null;
+                queue.Enqueue(node.right);
+                idx++; idx++;
+            }
+
+            return root;
+        }
+    }
+
+    public class _104_MaximumDepthOfBinaryTree
+    {
+        private int maxDepth;
+
+        public int MaxDepth(TreeNode root)
+        {
+            maxDepth = 0;
+
+            PreOrder(root, 0);
+
+            return maxDepth;
+        }
+
+        public void PreOrder(TreeNode cur, int depth)
+        {
+            if(cur is null)
+            {
+                maxDepth = maxDepth > depth ? maxDepth : depth;
+                return;
+            }
+
+            PreOrder(cur.left, depth + 1);
+            PreOrder(cur.right, depth + 1);
+
+            return;
+        }
+    }
+}
---
+++ Tests/TopInterview150/BinaryTree/104_MaximumDepthOfBinaryTree_Test.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode_C_.TopInterview150.BinaryTree;
+
+namespace Tests.TopInterview150.BinaryTree
+{
+    public class _104_MaximumDepthOfBinaryTree_Test
+    {
+        private readonly _104_MaximumDepthOfBinaryTree _algorithm;
+
+        public _104_MaximumDepthOfBinaryTree_Test()
+        {
+            _algorithm = new _104_MaximumDepthOfBinaryTree();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void MaxDepth_Input_Returns(List<int?> trees, int expectedResult)
+        {
+            // Assign
+            TreeNode root = TreeNode.GetTreeNode(trees);
+
+            // Act
+            var result = _algorithm.MaxDepth(root);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { new List<int?> { 3, 9, 20, null, null, 15, 7 }, 3 };
+            yield return new object[] { new List<int?> { 1, null, 2 }, 2 };        
+        }
+
+    }
+}
