d0efe6842de01801676adaf30ef55338b7df1e5d
ydongchoi <105weapon@gmail.com>
2024-07-09 11:13:26 -0700

    [feature] Added 098_ValidateBinarySearchTree Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/BinaryTree/098_ValidateBinarySearchTree.cs
@@ -0,0 +1,73 @@
+﻿
+// Link : https://leetcode.com/problems/validate-binary-search-tree/description/
+
+namespace LeetCode_C_.TopInterview150.BinaryTree
+{
+    public class _098_ValidateBinarySearchTree
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            // Make Queue
+            List<int> list = new List<int>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                TreeNode cur = queue.Dequeue();
+                list.Add(cur.val);
+
+                if (cur.left is not null) queue.Enqueue(cur.left);
+                if (cur.right is not null) queue.Enqueue(cur.right);
+            }
+
+            // Make BST using list
+            TreeNode bstTree = new TreeNode(list[0]);
+            for (int idx = 1; idx < list.Count; idx++)
+            {
+                bool isValid = MakeBstTree(root, bstTree, list[idx]);
+                if (isValid == false) return false;
+            }
+
+            return true;
+        }
+
+        public bool MakeBstTree(TreeNode root, TreeNode bstRoot, int value)
+        {
+
+            if (value < bstRoot.val)
+            {
+
+                if (bstRoot.left is null)
+                {
+                    Console.WriteLine($"left : {value}");
+
+                    if (root.left is null || root.left.val != value)
+                    {
+                        return false;
+                    }
+                    bstRoot.left = new TreeNode(value);
+
+                    return true;
+                }
+
+                return MakeBstTree(root.left, bstRoot.left, value);
+            }
+            else if (bstRoot.val < value)
+            {
+                Console.WriteLine($"right : {value}");
+
+                if (bstRoot.right is null)
+                {
+                    if (root.right is null || root.right.val != value) return false;
+                    bstRoot.right = new TreeNode(value);
+                    return true;
+                }
+
+                return MakeBstTree(root.right, bstRoot.right, value);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Tests/TopInterview150/BinaryTree/098_ValidateBinarySearchTree_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150.BinaryTree;
+
+namespace Tests.TopInterview150.BinaryTree
+{
+    public class _098_ValidateBinarySearchTree_Test
+    {
+        private readonly _098_ValidateBinarySearchTree _algorithm;
+
+        public _098_ValidateBinarySearchTree_Test()
+        {
+            _algorithm = new _098_ValidateBinarySearchTree();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void IsValidBST_Test(TreeNode root, bool expected)
+        {
+            // Act
+            var result = _algorithm.IsValidBST(root);
+            
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { TreeNode.GetTreeNode(new List<int?> { 2, 1, 3 }), true };
+            yield return new object[] { TreeNode.GetTreeNode(new List<int?> { 5, 1, 4, null, null, 3, 6 }), false };
+        }
+
+    }
+}
