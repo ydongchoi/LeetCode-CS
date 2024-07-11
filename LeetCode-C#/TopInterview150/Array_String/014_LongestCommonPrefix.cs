82a1f8a32bd4b1fa6b8036afa727bd6fcb143d5c
ydongchoi <105weapon@gmail.com>
2024-07-11 13:14:21 -0700

    [feature] Created 014_LongestCommonPrefix Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/Array_String/014_LongestCommonPrefix.cs
@@ -0,0 +1,30 @@
+﻿
+// Time Complexity : O(NM), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/longest-common-prefix/?envType=study-plan-v2&envId=top-interview-150
+
+using System.Text;
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _014_LongestCommonPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            StringBuilder commonPrefix = new StringBuilder();
+            int idx = 0;
+
+            while (true)
+            {
+                foreach(var str in strs)
+                {
+                    if (idx >= str.Length || str[idx] != strs[0][idx])
+                    {
+                        return commonPrefix.ToString();
+                    }
+                }
+
+                commonPrefix.Append(strs[0][idx++]);
+            }
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/014_LongestCommonPrefix_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _014_LongestCommonPrefix_Test
+    {
+        private readonly _014_LongestCommonPrefix _algorithm;
+
+        public _014_LongestCommonPrefix_Test()
+        {
+            _algorithm = new _014_LongestCommonPrefix();
+        }
+
+        [Theory]
+        [InlineData(new string[] { "flower", "flow", "flight" }, "fl")]
+        [InlineData(new string[] { "dog", "racecar", "car" }, "")]
+        [InlineData(new string[] { "ab", "a" }, "a")]
+        public void LongestCommonPrefix_InputStringArray_Returns(string[] strs, string expectedResult)
+        {
+            // Act
+            var result = _algorithm.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
