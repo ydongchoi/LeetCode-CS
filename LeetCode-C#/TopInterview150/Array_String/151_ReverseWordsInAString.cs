9378b75be5e548f3b434debce393ae5f995e7c7f
ydongchoi <105weapon@gmail.com>
2024-07-12 14:00:47 -0700

    [feature] Added ReverseWordsInAString Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/Array_String/151_ReverseWordsInAString.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/reverse-words-in-a-string/description/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _151_ReverseWordsInAString
+    {
+        public string ReverseWords(string s)
+        {
+            StringBuilder answer = new StringBuilder();
+            StringBuilder word = new StringBuilder();
+
+            for (int idx = 0; idx < s.Length; idx++)
+            {
+                if (s[idx] == ' ')
+                {
+                    if (word.Length != 0)
+                    {
+                        answer.Insert(0, word);
+                        answer.Insert(0, ' ');
+                    }
+
+                    word.Clear();
+                    continue;
+                }
+                else
+                {
+                    word.Append(s[idx]);
+                }
+            }
+
+            answer.Insert(0, word);
+            answer.Insert(0, ' ');
+
+            return answer.ToString().Trim();
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/151_ReverseWordsInAString_Test.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _151_ReverseWordsInAString_Test
+    {
+        private readonly _151_ReverseWordsInAString _algorithm;
+
+        public _151_ReverseWordsInAString_Test()
+        {
+            _algorithm = new _151_ReverseWordsInAString();
+        }
+
+        [Theory]
+        [InlineData("the sky is blue", "blue is sky the")]
+        [InlineData("  hello world  ", "world hello")]
+        [InlineData("a good   example", "example good a")]
+        public void ReverseWords_Input_Returns(string s, string expected)
+        {
+            // Act
+            var result = _algorithm.ReverseWords(s);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
