2ecd2aa62b7e17279ce003761459b8265ceedf39
ydongchoi <105weapon@gmail.com>
2024-07-15 15:31:51 -0700

    [feture] Added 078_Subsets Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/BackTracking/078_Subsets.cs
@@ -0,0 +1,47 @@
+﻿
+// Time Complexity : O(2N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/subsets/description/
+
+namespace LeetCode_C_.TopInterview150.BackTracking
+{
+    public class _078_Subsets
+    {
+        public IList<int> singleAnswer;
+        public IList<IList<int>> answer;
+
+        public _078_Subsets()
+        {
+            singleAnswer = new List<int>();
+            answer = new List<IList<int>>();
+        }
+
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            answer.Add(new List<int>());
+
+            for (int idx = 0; idx < nums.Length; ++idx)
+            {
+                singleAnswer = new List<int>();
+                FindSubset(0, idx, 0, nums.Length, nums);
+            }
+
+            return answer;
+        }
+
+        public void FindSubset(int curCnt, int setCnt, int nxtIdx, int numsLength, int[] nums)
+        {
+            if (curCnt == setCnt + 1)
+            {
+                answer.Add(new List<int>(singleAnswer));
+                return;
+            }
+
+            for (int idx = nxtIdx; idx < numsLength; ++idx)
+            {
+                singleAnswer.Add(nums[idx]);
+                FindSubset(curCnt + 1, setCnt, idx + 1, numsLength, nums);
+                singleAnswer.RemoveAt(singleAnswer.Count - 1);
+            }
+        }
+    }
+}
---
+++ Tests/TopInterview150/BackTracking/078_Subsets_Test.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode_C_.TopInterview150.BackTracking;
+
+namespace Tests.TopInterview150.BackTracking
+{
+    public class _078_Subsets_Test
+    {
+        private readonly _078_Subsets _algorithm;
+
+        public _078_Subsets_Test()
+        {
+            _algorithm = new _078_Subsets();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Subsets_Test(int[] nums, int[][] expected)
+        {
+            var result = _algorithm.Subsets(nums);
+
+            for (var i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i].ToList(), result[i]);
+            }
+        }
+
+        public static TheoryData<int[], int[][]> TestData()
+        {
+            return new TheoryData<int[], int[][]>
+                {
+                    { new int[] { 1, 2, 3 }, new int[][] { new int[] {}, new int[] { 1 }, new int[] { 2 }, new int[] { 3 }, new int[] { 1, 2 }, new int[] { 1, 3 }, new int[] { 2, 3 }, new int[] { 1, 2, 3 } } },
+                    { new int[] { 0 }, new int[][] { new int[] { }, new int[] { 0 } } }
+                };
+        }
+    }
+}
