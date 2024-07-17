1750272f8e02407e59d00a5817cad7bc608b447d
ydongchoi <105weapon@gmail.com>
2024-07-17 14:01:54 -0700

    [feature] Added 102_BinaryTreeLevelOrderTraversal Soltuion and Unit test.

---
+++ LeetCode-C#/TopInterview150/BinaryTreeBFS/102_BinaryTreeLevelOrderTraversal.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode_C_.TopInterview150.BinaryTree;
+
+// Time Complexity : O(NM), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/binary-tree-level-order-traversal/description/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150.BinaryTreeBFS
+{
+    public class _102_BinaryTreeLevelOrderTraversal
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            IList<IList<int>> answer = new List<IList<int>>();
+            Queue<(TreeNode, int)> queue = new Queue<(TreeNode, int)>();
+            queue.Enqueue((root, 1));
+
+            while (queue.Count > 0)
+            {
+                var curData = queue.Dequeue();
+                TreeNode cur = curData.Item1; int depth = curData.Item2;
+                if (cur is null) continue;
+
+                if (answer.Count < depth)
+                {
+                    answer.Add(new List<int>());
+                    answer[depth - 1].Add(cur.val);
+                }
+                else answer[depth - 1].Add(cur.val);
+
+                if (cur.left is not null) queue.Enqueue((cur.left, depth + 1));
+                if (cur.right is not null) queue.Enqueue((cur.right, depth + 1));
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/TopInterview150/BinaryTreeBFS/102_BinaryTreeLevelOrderTraversal_Test.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode_C_.TopInterview150.BinaryTree;
+using LeetCode_C_.TopInterview150.BinaryTreeBFS;
+
+
+namespace Tests.TopInterview150.BinaryTreeBFS
+{
+    public class _102_BinaryTreeLevelOrderTraversal_Test
+    {
+        private readonly _102_BinaryTreeLevelOrderTraversal _algorithm;
+
+        public _102_BinaryTreeLevelOrderTraversal_Test()
+        {
+            _algorithm = new _102_BinaryTreeLevelOrderTraversal();
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void LevelOrder_Test(TreeNode root, IList<IList<int>> expected)
+        {
+            var result = _algorithm.LevelOrder(root);
+
+            Assert.Equal(expected.Count, result.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], result[i]);
+            }
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            yield return new object[] { TreeNode.GetTreeNode(new List<int?> { 3, 9, 20, null, null, 15, 7 }), new List<IList<int>> { new List<int> { 3 }, new List<int> { 9, 20 }, new List<int> { 15, 7 } } };
+            yield return new object[] { TreeNode.GetTreeNode(new List<int?> { 1 }), new List<IList<int>> { new List<int> { 1 } } };
+            yield return new object[] { TreeNode.GetTreeNode(new List<int?> { 1, 2, 3, 4, 5, 6, 7 }), new List<IList<int>> { new List<int> { 1 }, new List<int> { 2, 3 }, new List<int> { 4, 5, 6, 7 } } };
+        }
+    }
+}
