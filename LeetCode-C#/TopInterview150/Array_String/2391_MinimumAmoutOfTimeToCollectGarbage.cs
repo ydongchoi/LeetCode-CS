3e15276b7ccdb0bf6914b19c5ce943ed8efe030d
ydongchoi <105weapon@gmail.com>
2024-07-25 15:47:32 -0700

    [feature] Added 2391_MinimumAmountOfTimeToCollectGarbage Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/Array_String/2391_MinimumAmoutOfTimeToCollectGarbage.cs
@@ -0,0 +1,39 @@
+﻿
+// Time Complexity : O(NM), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/description/
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _2391_MinimumAmoutOfTimeToCollectGarbage
+    {
+        public int GarbageCollection(string[] garbage, int[] travel)
+        {
+            // P, G, M
+            char[] trucks = new char[] { 'P', 'G', 'M' };
+            int costTime = 0;
+
+            for (int idx = 0; idx < trucks.Length; ++idx)
+            {
+                char type = trucks[idx];
+                int travelTime = 0;
+
+                for (int idx1 = 0; idx1 < garbage.Length; idx1++)
+                {
+                    if (!garbage[idx1].Contains(type))
+                    {
+                        if (idx1 != 0) travelTime = travelTime + travel[idx1 - 1];
+                        continue;
+                    }
+
+                    int minute = garbage[idx1].ToCharArray().Count(cc => cc == type);
+                    if (idx1 != 0) travelTime = travelTime + travel[idx1 - 1];
+
+                    costTime = costTime + minute + travelTime;
+                    travelTime = 0;
+                }
+            }
+
+            return costTime;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/2391_MinimumAmoutOfTimeToCollectGarbage_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _2391_MinimumAmoutOfTimeToCollectGarbage_Test
+    {
+        private readonly _2391_MinimumAmoutOfTimeToCollectGarbage _algorithm;
+
+        public _2391_MinimumAmoutOfTimeToCollectGarbage_Test()
+        {
+            _algorithm = new _2391_MinimumAmoutOfTimeToCollectGarbage();
+        }
+
+        [Theory]
+        [InlineData(new string[] { "G", "P", "GP", "GG" }, new int[] { 2, 4, 3 }, 21)]
+        [InlineData(new string[] { "MMM", "PGM", "GP" }, new int[] { 3, 10 }, 37)]
+        public void GarbageCollection_Input_Returns(string[] garbage, int[] travel, int expected)
+        {
+            // Act
+            var actual = _algorithm.GarbageCollection(garbage, travel);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
