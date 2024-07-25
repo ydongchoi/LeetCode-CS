e33cc4d52f8e33eff757087f3264d7322866f32c
ydongchoi <105weapon@gmail.com>
2024-07-25 13:50:51 -0700

    [feature] Added 83_RemoveDuplicatesFromSortedList Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/LinkedList/83_RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,37 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+
+
+namespace LeetCode_C_.TopInterview150.LinkedList
+{
+    public class _83_RemoveDuplicatesFromSortedList
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head is null) return null;
+
+            HashSet<int> memoization = new HashSet<int>();
+
+            ListNode prev = head; memoization.Add(prev.val);
+            ListNode cur = head.next;
+
+            while (cur is not null)
+            {
+                if (memoization.Contains(cur.val))
+                {
+                    prev.next = cur.next;
+                }
+                else
+                {
+                    memoization.Add(cur.val);
+                    prev = prev.next;
+                }
+
+                cur = cur.next;
+            }
+
+            return head;
+        }
+    }
+}
---
+++ Tests/TopInterview150/LinkedList/83_RemoveDuplicatesFromSortedList_Test.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode_C_.TopInterview150.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.LinkedList
+{
+    public class _83_RemoveDuplicatesFromSortedList_Test
+    {
+        private readonly _83_RemoveDuplicatesFromSortedList _algorithm;
+
+        public _83_RemoveDuplicatesFromSortedList_Test()
+        {
+            _algorithm = new _83_RemoveDuplicatesFromSortedList();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 1, 2 }, new int[] { 1, 2 })]
+        [InlineData(new int[] { 1, 1, 2, 3, 3 }, new int[] { 1, 2, 3 })]
+        public void DeleteDuplicates_Input_Returns(int[] nums, int[] expectedResult)
+        {
+            var head = new ListNode(nums[0]);
+            var cur = head;
+            for (int idx = 1; idx < nums.Length; idx++)
+            {
+                cur.next = new ListNode(nums[idx]);
+                cur = cur.next;
+            }
+
+            ListNode result = _algorithm.DeleteDuplicates(head);
+
+            foreach (var expResult in expectedResult)
+            {
+                Assert.Equal(expResult, result.val);
+                result = result.next;
+            }
+        }
+    }
+}
