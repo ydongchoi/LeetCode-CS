a524616a10d25ba9e6dcea5479dfda1690a16507
ydongchoi <105weapon@gmail.com>
2024-07-29 15:46:36 -0700

    [feature] Added 70_ClimbingStairs Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/DynamicProgramming/70_ClimbingStairs.cs
@@ -0,0 +1,34 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/climbing-stairs/
+
+namespace LeetCode_C_.TopInterview150.DynamicProgramming
+{
+    public class _70_ClimbingStairs
+    {
+        int[] memo;
+
+        public int ClimbStairs(int n)
+        {
+            memo = new int[50];
+
+            return DynamicProgramming(0, n);
+        }
+
+        public int DynamicProgramming(int cur, int n)
+        {
+            if (cur == n)
+            {
+                return 1;
+            }
+            else if (cur > n)
+            {
+                return 0;
+            }
+
+            memo[cur] = memo[cur] != 0 ? memo[cur] : (DynamicProgramming(cur + 1, n) + DynamicProgramming(cur + 2, n));
+
+            return memo[cur];
+        }
+    }
+}
---
+++ Tests/TopInterview150/DynamicProgramming/70_ClimbingStairs_Test.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode_C_.TopInterview150.DynamicProgramming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.DynamicProgramming
+{
+    public class _70_ClimbingStairs_Test
+    {
+        private readonly _70_ClimbingStairs _algorithm;
+
+        public _70_ClimbingStairs_Test()
+        {
+            _algorithm = new _70_ClimbingStairs();
+        }
+
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        public void ClimbStairs_Input_Returns(int n, int expected)
+        {
+            var result = _algorithm.ClimbStairs(n);
+            Assert.Equal(expected, result);
+        }
+    }
+}
