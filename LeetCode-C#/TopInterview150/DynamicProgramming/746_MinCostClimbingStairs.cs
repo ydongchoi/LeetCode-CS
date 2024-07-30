8dbb251a820a798b4176d909fbb6409502609892
ydongchoi <105weapon@gmail.com>
2024-07-30 15:28:03 -0700

    [feature] Added 746_MinCostClimbingStairs Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/DynamicProgramming/746_MinCostClimbingStairs.cs
@@ -0,0 +1,25 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/min-cost-climbing-stairs/
+
+namespace LeetCode_C_.TopInterview150.DynamicProgramming
+{
+    public class _746_MinCostClimbingStairs
+    {
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            int top = cost.Length;
+            int[] memo = new int[top + 1];
+
+            for (int idx = 2; idx <= cost.Length; ++idx)
+            {
+                int first = memo[idx - 1] + cost[idx - 1];
+                int second = memo[idx - 2] + cost[idx - 2];
+
+                memo[idx] = Math.Min(first, second);
+            }
+
+            return memo[top];
+        }
+    }
+}
---
+++ Tests/TopInterview150/DynamicProgramming/746_MinCostClimbingStairs_Test.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode_C_.TopInterview150.DynamicProgramming;
+
+namespace Tests.TopInterview150.DynamicProgramming
+{
+    public class _746_MinCostClimbingStairs_Test
+    {
+        private readonly _746_MinCostClimbingStairs _algorithm;
+
+        public _746_MinCostClimbingStairs_Test()
+        {
+            _algorithm = new _746_MinCostClimbingStairs();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 10, 15, 20 }, 15)]
+        [InlineData(new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }, 6)]
+        public void MinCostClimbingStairs_Input_Returns(int[] cost, int expected)
+        {
+            var result = _algorithm.MinCostClimbingStairs(cost);
+            Assert.Equal(expected, result);
+        }
+    }
+}
