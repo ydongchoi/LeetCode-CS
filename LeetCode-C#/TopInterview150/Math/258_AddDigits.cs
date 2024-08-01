aac328383e43c21da26010c112d2469b75140a0a
ydongchoi <105weapon@gmail.com>
2024-08-01 15:55:16 -0700

    [feature] Add 258_AddDigits Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/Math/258_AddDigits.cs
@@ -0,0 +1,29 @@
+﻿
+// Time Complexity : O(1), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/add-digits/description/
+
+namespace LeetCode_C_.TopInterview150.Math
+{
+    public class _258_AddDigits
+    {
+        public int AddDigits(int num)
+        {
+
+            while (num / 10 != 0)
+            {
+
+                int nxtNum = num;
+                num = 0;
+
+                while (nxtNum != 0)
+                {
+                    num = num + (nxtNum % 10);
+
+                    nxtNum /= 10;
+                }
+            }
+
+            return num;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Math/258_AddDigits_Test.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode_C_.TopInterview150.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.Math
+{
+    public class _258_AddDigits_Test
+    {
+        private readonly _258_AddDigits _algorithm;
+
+        public _258_AddDigits_Test()
+        {
+            _algorithm = new _258_AddDigits();
+        }
+
+        [Theory]
+        [InlineData(38, 2)]
+        [InlineData(0, 0)]
+        public void AddDigits_Input_Returns(int num, int expected)
+        {
+            // Act
+            var actual = _algorithm.AddDigits(num);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
