00276a555fc2798450cdc37578cf5999ea8ba721
ydongchoi <105weapon@gmail.com>
2024-08-06 12:39:05 -0700

    [feature] 605_CanPlaceFlowerscs Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/Array_String/605_CanPlaceFlowerscs.cs
@@ -0,0 +1,42 @@
+﻿
+// Time Complexity : O(M + N), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/can-place-flowers/
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _605_CanPlaceFlowerscs
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            if (flowerbed.Length == 1)
+            {
+                return flowerbed[0] == 0 ? n <= 1 : n <= 0;
+            }
+
+            int possible = 0;
+            if (flowerbed[0] == 0 && flowerbed[1] == 0)
+            {
+                flowerbed[0] = 1; possible += 1;
+            }
+
+            for (int idx = 1; idx < flowerbed.Length - 1; idx++)
+            {
+                int prev = idx - 1; int next = idx + 1;
+
+                if (flowerbed[prev] == 0 && flowerbed[next] == 0 && flowerbed[idx] == 0)
+                {
+                    flowerbed[idx] = 1; possible += 1;
+                }
+            }
+
+            if (flowerbed[flowerbed.Length - 2] == 0 && flowerbed[flowerbed.Length - 1] == 0)
+            {
+                flowerbed[flowerbed.Length - 1] = 1; possible += 1;
+            }
+
+            if (possible >= n) return true;
+            else return false;
+
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/605_CanPlaceFlowerscs_Test.cs
@@ -0,0 +1,24 @@
+﻿
+using LeetCode_C_.TopInterview150.Array_String;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _605_CanPlaceFlowerscs_Test
+    {
+        private readonly _605_CanPlaceFlowerscs _algorithm;
+
+        public _605_CanPlaceFlowerscs_Test()
+        {
+            _algorithm = new _605_CanPlaceFlowerscs();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 0, 0, 0, 1 }, 1, true)]
+        [InlineData(new int[] { 1, 0, 0, 0, 1 }, 2, false)]
+        public void CanPlaceFlowers_Input_Returns(int[] flowerbed, int n, bool expectedResult)
+        {
+            var result = _algorithm.CanPlaceFlowers(flowerbed, n);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
