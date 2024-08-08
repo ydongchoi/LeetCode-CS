701395b89f1f450e177c7000eda50664b406a862
ydongchoi <105weapon@gmail.com>
2024-08-08 16:02:00 -0700

    [feature] Added 3033_ModifyTheMatrix Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/Array_String/3033_ModifyTheMatrix.cs
@@ -0,0 +1,37 @@
+﻿
+// Time Complexity : O(M*N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/modify-the-matrix/description/
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _3033_ModifyTheMatrix
+    {
+        public int[][] ModifiedMatrix(int[][] matrix)
+        {
+
+            for (int i = 0; i < matrix[0].Length; ++i)
+            {
+                List<int> posMem = new List<int>();
+                int max = 0;
+
+                for (int j = 0; j < matrix.Length; ++j)
+                {
+                    if (matrix[j][i] == -1)
+                    {
+                        posMem.Add(j);
+                        continue;
+                    }
+                    max = max > matrix[j][i] ? max : matrix[j][i];
+                }
+
+                foreach (int elem in posMem)
+                {
+                    matrix[elem][i] = max;
+                }
+
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/3033_ModifyTheMatrix_Test.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _3033_ModifyTheMatrix_Test
+    {
+        private readonly _3033_ModifyTheMatrix _algorithm;
+
+        public _3033_ModifyTheMatrix_Test()
+        {
+            _algorithm = new _3033_ModifyTheMatrix();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void ModifiedMatrix_Input_Returns(int[][] matrix, int[][] expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = _algorithm.ModifiedMatrix(matrix);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[]
+            {
+                    new int[][] { new int[] { 1, 2, -1 }, new int[] { 4, -1, 6 }, new int[] { 7, 8, 9 } },
+                    new int[][] { new int[] { 1, 2, 9 }, new int[] { 4, 8, 6 }, new int[] { 7, 8, 9 } }
+            };
+            yield return new object[]
+            {
+                    new int[][] { new int[] { 3, -1 }, new int[] { 5, 2 } },
+                    new int[][] { new int[] { 3, 2 }, new int[] { 5, 2 } }
+            };
+        }
+
+    }
+}
