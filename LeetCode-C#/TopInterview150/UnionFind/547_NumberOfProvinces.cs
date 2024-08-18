1139246a41878773c3bc1d0b805a8550158b7226
ydongchoi <105weapon@gmail.com>
2024-08-17 19:31:35 -0700

    [feature] Added 547_NumberOfProvinces Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/UnionFind/547_NumberOfProvinces.cs
@@ -0,0 +1,62 @@
+﻿
+// Time Complexity : O(N^2), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/number-of-provinces/description/
+
+namespace LeetCode_C_.TopInterview150.UnionFind
+{
+    public class _547_NumberOfProvinces
+    {
+        private int[] parent;
+
+        public int FindCircleNum(int[][] isConnected)
+        {
+            int n = isConnected.Length;
+
+            parent = new int[n];
+            // Initialization of Disjoint Sets
+            for (int idx = 0; idx < n; ++idx)
+            {
+                parent[idx] = idx;
+            }
+
+            // Make Graph using Edge and Union
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (isConnected[i][j] == 1)
+                    {
+                        Union(i, j);
+                    }
+                }
+            }
+
+            // Find the number of root
+            int count = 0;
+            for (var i = 0; i < parent.Length; i++)
+            {
+                if (Find(i) == i) count++;
+            }
+            return count;
+        }
+
+        public int Find(int node)
+        {
+            if (parent[node] == node)
+            {
+                return node;
+            }
+            return parent[node] = Find(parent[node]);
+        }
+
+        public void Union(int x, int y)
+        {
+            int rootX = Find(x);
+            int rootY = Find(y);
+            if (rootX != rootY)
+            {
+                parent[rootY] = rootX;
+            }
+        }
+    }
+}
---
+++ Tests/TopInterview150/UnionFind/547_NumberOfProvinces_Test.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode_C_.TopInterview150.UnionFind;
+
+namespace Tests.TopInterview150.UnionFind
+{
+    public class _547_NumberOfProvinces_Test
+    {
+        private readonly _547_NumberOfProvinces _algorithm;
+
+        public _547_NumberOfProvinces_Test()
+        {
+            _algorithm = new _547_NumberOfProvinces();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void FindCircleNum_Test(int[][] isConnected, int expected)
+        {
+            var result = _algorithm.FindCircleNum(isConnected);
+            Assert.Equal(expected, result);
+        }
+
+        public static TheoryData<int[][], int> TestData()
+        {
+            var testData = new TheoryData<int[][], int>();
+            testData.Add(new int[][] { new int[] { 1, 1, 0 }, new int[] { 1, 1, 0 }, new int[] { 0, 0, 1 } }, 2);
+            testData.Add(new int[][] { new int[] { 1, 0, 0 }, new int[] { 0, 1, 0 }, new int[] { 0, 0, 1 } }, 3);
+            return testData;
+        }
+    }
+}
