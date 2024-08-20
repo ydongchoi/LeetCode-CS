54b8175ae48b751cc1f2808eb1c80aee664ffc37
ydongchoi <105weapon@gmail.com>
2024-08-19 20:20:54 -0700

    [feature] Added 136_SingleNumber Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/Array_String/136_SingleNumber.cs
@@ -0,0 +1,21 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/single-number/
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _136_SingleNumber
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var answer = 0;
+
+            for (int idx = 0; idx < nums.Length; ++idx)
+            {
+                answer ^= nums[idx];
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/136_SingleNumber_Test.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _136_SingleNumber_Test
+    {
+        private readonly _136_SingleNumber _algorithm;
+
+        public _136_SingleNumber_Test()
+        {
+            _algorithm = new _136_SingleNumber();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 2, 1 }, 1)]
+        [InlineData(new int[] { 4, 1, 2, 1, 2 }, 4)]
+        [InlineData(new int[] { 1 }, 1)]
+        public void SingleNumber_Test(int[] nums, int expected)
+        {
+            var result = _algorithm.SingleNumber(nums);
+            Assert.Equal(expected, result);
+        }
+    }
+}
