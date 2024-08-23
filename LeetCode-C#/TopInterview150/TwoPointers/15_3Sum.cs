46d8fd1ca1982a2ab1a9f609a09c9c3f8ea5bd18
ydongchoi <105weapon@gmail.com>
2024-08-23 15:31:26 -0700

    [feature] Added 15_3Sum Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/TwoPointers/15_3Sum.cs
@@ -0,0 +1,57 @@
+﻿
+// Time Complexity : O(NlogN + N^2), Spacious Complexity : O(N^2)
+// Link : https://leetcode.com/problems/3sum/description/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150.TwoPointers
+{
+    public class _15_3Sum
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            List<List<int>> answer = new List<List<int>>();
+            Dictionary<int, List<int>> dic = new Dictionary<int, List<int>>();
+            HashSet<string> isVisited = new HashSet<string>();
+
+            // nlogn
+            Array.Sort(nums);
+
+            // n, n
+            for (int idx = 0; idx < nums.Length; ++idx)
+            {
+                if (dic.TryGetValue(nums[idx], out List<int> val))
+                {
+                    if (val.Count < 3) val.Add(idx);
+                }
+                else
+                {
+                    dic.Add(nums[idx], new List<int> { idx });
+                }
+
+            }
+
+            // n^2
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                for (int j = i + 1; j < nums.Length; ++j)
+                {
+                    var tmp = -(nums[i] + nums[j]);
+                    string combi = nums[i].ToString() + nums[j].ToString() + tmp.ToString();
+                    if (isVisited.Contains(combi)) continue;
+
+                    if (dic.TryGetValue(tmp, out List<int> val))
+                    {
+                        foreach (var elem in val)
+                        {
+                            if (i == elem || j == elem || elem < i || elem < j || isVisited.Contains(combi)) continue;
+
+                            isVisited.Add(combi);
+                            answer.Add(new List<int> { nums[i], nums[j], tmp });
+                        }
+                    }
+                }
+            }
+
+            return answer.ToArray();
+        }
+    }
+}
---
+++ Tests/TopInterview150/TwoPointers/15_3Sum_Test.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode_C_.TopInterview150.TwoPointers;
+
+namespace Tests.TopInterview150.TwoPointers
+{
+    public class _15_3Sum_Test
+    {
+        private readonly _15_3Sum _algorithm;
+
+        public _15_3Sum_Test()
+        {
+            _algorithm = new _15_3Sum();
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void ThreeSum_Test(int[] nums, int[][] expected)
+        {
+            var result = _algorithm.ThreeSum(nums);
+            Assert.Equal(expected.Length, result.Count);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], result[i]);
+            }
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            yield return new object[] { new[] { -1, 0, 1, 2, -1, -4 }, new[] { new[] { -1, -1, 2 }, new[] { -1, 0, 1 } } };
+            yield return new object[] { new int[] { }, new int[][] { } };
+            yield return new object[] { new[] { 0 }, new int[][] { } };
+        }
+    }
+}
