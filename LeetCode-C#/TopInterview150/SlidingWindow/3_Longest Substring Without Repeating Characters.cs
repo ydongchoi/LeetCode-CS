1bde9d8a9ab15bbb435962aa85df3d58cccb0ca7
ydongchoi <105weapon@gmail.com>
2024-09-03 13:43:50 -0700

    [feature] Added 3_Longest_Substring_Without_Repeating_Characters Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/SlidingWindow/3_Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,47 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+namespace LeetCode_C_.TopInterview150.SlidingWindow
+{
+    public class _3_Longest_Substring_Without_Repeating_Characters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            int answer = 0;
+            int tmpSubString = 0; int start = 0;
+
+            for (int idx = 0; idx < s.Length; ++idx)
+            {
+                char elem = s[idx];
+
+                if (dic.TryGetValue(elem, out int val))
+                {
+                    answer = answer > tmpSubString ? answer : tmpSubString;
+                    dic.Clear();
+
+                    tmpSubString = 0;
+                    for (int i = val + 1; i <= idx; ++i)
+                    {
+                        if (!dic.ContainsKey(s[i]))
+                        {
+                            dic.Add(s[i], i); tmpSubString++;
+                        }
+                    }
+
+                    start = val + 1;
+
+                    continue;
+                }
+
+                dic.Add(elem, idx);
+                tmpSubString++;
+            }
+
+            answer = answer > tmpSubString ? answer : tmpSubString;
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/TopInterview150/SlidingWindow/3_Longest Substring Without Repeating CharactersTest.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode_C_.TopInterview150.SlidingWindow;
+
+namespace Tests.TopInterview150.SlidingWindow
+{
+    public class _3_Longest_Substring_Without_Repeating_CharactersTest
+    {
+        private readonly _3_Longest_Substring_Without_Repeating_Characters _algorithm;
+
+        public _3_Longest_Substring_Without_Repeating_CharactersTest()
+        {
+            _algorithm = new _3_Longest_Substring_Without_Repeating_Characters();
+        }
+
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        public void LengthOfLongestSubstring_Input_Returns(string s, int expected)
+        {
+            var result = _algorithm.LengthOfLongestSubstring(s);
+            Assert.Equal(expected, result);
+        }
+    }
+}
