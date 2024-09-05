8ea3c05fe61ecf47beb1ff10db6b707c421c9635
ydongchoi <105weapon@gmail.com>
2024-09-05 11:28:36 -0700

    [feature] Added 819_MostCommonWord Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/Array_String/819_MostCommonWord.cs
@@ -0,0 +1,47 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// https://leetcode.com/problems/most-common-word/description/
+// Split, Dictionary, HashSet
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _819_MostCommonWord
+    {
+        public string MostCommonWord(string paragraph, string[] banned)
+        {
+            // Split the paragraph using charSeperators
+            char[] charSeperators = new char[] { ' ', '!', '?', ',', ';', '.', '\'' };
+            string[] words = paragraph.Split(charSeperators, StringSplitOptions.RemoveEmptyEntries);
+
+            // Stored banned to HashSet for finding banned word more fastly
+            HashSet<string> bannedMemo = new HashSet<string>();
+            foreach (var elem in banned) bannedMemo.Add(elem);
+
+            // Find the most frequent word in paragraph.
+            Dictionary<string, int> wordDic = new Dictionary<string, int>();
+            int maxWordCnt = 0;
+            string answer = "";
+
+            foreach (var word in words)
+            {
+                string lowerWord = word.ToLower();
+                if (bannedMemo.Contains(lowerWord)) continue;
+
+                if (wordDic.TryGetValue(lowerWord, out int wordCnt))
+                {
+                    wordDic[lowerWord]++;
+                    maxWordCnt = maxWordCnt > wordDic[lowerWord] ? maxWordCnt : wordDic[lowerWord];
+                    answer = maxWordCnt > wordDic[lowerWord] ? answer : lowerWord;
+
+                }
+                else
+                {
+                    wordDic.Add(lowerWord, 1);
+                    answer = maxWordCnt > 1 ? answer : lowerWord;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/819_MostCommonWordTest.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _819_MostCommonWordTest
+    {
+        private readonly _819_MostCommonWord _algorithm;
+
+        public _819_MostCommonWordTest()
+        {
+            _algorithm = new _819_MostCommonWord();
+        }
+
+        [Theory]
+        [InlineData("Bob hit a ball, the hit BALL flew far after it was hit.", new[] { "hit" }, "ball")]
+        [InlineData("a, a, a, a, b,b,b,c, c", new[] { "a" }, "b")]
+        public void MostCommonWordTest(string paragraph, string[] banned, string expected)
+        {
+            var result = _algorithm.MostCommonWord(paragraph, banned);
+            Assert.Equal(expected, result);
+        }
+    }
+}
