6bf3f9de110cf5207970c4f058a0f7b3f7d30b37
ydongchoi <105weapon@gmail.com>
2024-09-06 14:47:29 -0700

    [feature] Added 2841_MaximumSumOfAlmostUniqueSubarray Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/SlidingWindow/2841_MaximumSumOfAlmostUniqueSubarray.cs
@@ -0,0 +1,52 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(M)
+// https://leetcode.com/problems/maximum-sum-of-almost-unique-subarray/description/
+
+namespace LeetCode_C_.TopInterview150.SlidingWindow
+{
+    public class _2841_MaximumSumOfAlmostUniqueSubarray
+    {
+        public long MaxSum(IList<int> nums, int m, int k)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            long tmpVal = 0;
+            long answer = 0;
+
+            for (int idx = 0; idx < k; ++idx)
+            {
+                if (dic.TryGetValue(nums[idx], out int val))
+                {
+                    dic[nums[idx]]++;
+                }
+                else
+                {
+                    dic.Add(nums[idx], 1);
+                }
+                tmpVal += nums[idx];
+            }
+
+            if (dic.Count >= m) answer = tmpVal;
+
+            for (int idx = k; idx < nums.Count; ++idx)
+            {
+                tmpVal = tmpVal - nums[idx - k] + nums[idx];
+
+                if (dic[nums[idx - k]] > 1)
+                {
+                    dic[nums[idx - k]]--;
+                }
+                else
+                {
+                    dic.Remove(nums[idx - k]);
+                }
+
+                if (dic.ContainsKey(nums[idx])) dic[nums[idx]]++;
+                else dic.Add(nums[idx], 1);
+
+                if (dic.Count >= m) answer = answer > tmpVal ? answer : tmpVal;
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/TopInterview150/SlidingWindow/2841_MaximumSumOfAlmostUniqueSubarrayTest.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode_C_.TopInterview150.SlidingWindow;
+
+namespace Tests.TopInterview150.SlidingWindow
+{
+    public class _2841_MaximumSumOfAlmostUniqueSubarrayTest
+    {
+        private readonly _2841_MaximumSumOfAlmostUniqueSubarray _algortihm;
+
+        public _2841_MaximumSumOfAlmostUniqueSubarrayTest()
+        {
+            _algortihm = new _2841_MaximumSumOfAlmostUniqueSubarray();
+        }
+
+        [Theory]
+        [InlineData(new[] { 2, 6, 7, 3, 1, 7 }, 3, 4, 18)]
+        [InlineData(new[] { 5, 9, 9, 2, 4, 5, 4 }, 1, 3, 23)]
+        public void MaxSumTest_Input_Returns(int[] nums, int m, int k, long expected)
+        {
+            var result = _algortihm.MaxSum(nums, m, k);
+            Assert.Equal(expected, result);
+        }
+    }
+}
