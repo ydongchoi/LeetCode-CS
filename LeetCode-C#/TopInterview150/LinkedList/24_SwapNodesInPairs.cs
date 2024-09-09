7d4efdb91cc649b767fece8d868772eaaed8fa7d
ydongchoi <105weapon@gmail.com>
2024-09-09 12:17:35 -0700

    [feature] Added 24_SwapNodesInPairs Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/LinkedList/24_SwapNodesInPairs.cs
@@ -0,0 +1,37 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// https://leetcode.com/problems/swap-nodes-in-pairs/
+
+namespace LeetCode_C_.TopInterview150.LinkedList
+{
+    public class _24_SwapNodesInPairs
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head is null || head.next is null) return head;
+
+            ListNode prev = head;
+            ListNode cur = head;
+            ListNode answer = cur.next;
+
+            while (cur is not null)
+            {
+                if (cur.next is not null)
+                {
+                    ListNode tmp = cur.next;
+
+                    cur.next = cur.next.next;
+                    tmp.next = cur;
+
+                    if (cur != head) prev.next = tmp;
+                }
+                else break;
+
+                prev = cur;
+                cur = cur.next;
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/TopInterview150/LinkedList/24_SwapNodesInPairs_Test.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode_C_.TopInterview150.LinkedList;
+using Xunit.Sdk;
+
+namespace Tests.TopInterview150.LinkedList
+{
+    public class _24_SwapNodesInPairs_Test
+    {
+        private readonly _24_SwapNodesInPairs _algorithm;
+
+        public _24_SwapNodesInPairs_Test()
+        {
+            _algorithm = new _24_SwapNodesInPairs();
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4 }, new[] { 2, 1, 4, 3 })]
+        [InlineData(new[] { 1, 2, 3 }, new[] { 2, 1, 3 })]
+        [InlineData(new[] { 1 }, new[] { 1 })]
+        public void SwapPairs_Test(int[] input, int[] output)
+        {
+            // Arrange
+            var head = new ListNode(input[0]);
+            var cur = head;
+            for (int idx = 1; idx < input.Length; idx++)
+            {
+                cur.next = new ListNode(input[idx]);
+                cur = cur.next;
+            }
+
+            // Act
+            ListNode result = _algorithm.SwapPairs(head);
+
+
+            // Assert
+            for(int idx = 0; idx < output.Length; idx++)
+            {
+                Assert.Equal(output[idx], result.val);
+                result = result.next;
+            }
+        }
+    }
+}
