a07040302c3768a064d017f58b2736b5f9c2946a
ydongchoi <105weapon@gmail.com>
2024-09-10 15:09:59 -0700

    [feature] Added 3110_ScoreOfAString Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/Array_String/3110_ScoreOfAString.cs
@@ -0,0 +1,21 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(1)
+// https://leetcode.com/problems/score-of-a-string/
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _3110_ScoreOfAString
+    {
+        public int ScoreOfString(string s)
+        {
+            int answer = 0;
+
+            for (int idx = 0; idx < s.Length - 1; ++idx)
+            {
+                answer += System.Math.Abs(s[idx] - s[idx + 1]);
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/3110_ScoreOfAString_Test.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _3110_ScoreOfAString_Test
+    {
+        private readonly _3110_ScoreOfAString _algorithm;
+
+        public _3110_ScoreOfAString_Test()
+        {
+            _algorithm = new _3110_ScoreOfAString();
+        }
+
+        [Theory]
+        [InlineData("hello", 13)]
+        [InlineData("zaz", 50)]
+        public void ScoreOfString_Test(string s, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = _algorithm.ScoreOfString(s);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
