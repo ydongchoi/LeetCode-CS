56722802224071ad6799bb0f7026d252ec04e7de
ydongchoi <105weapon@gmail.com>
2024-09-11 17:04:43 -0700

    [feature] Added 229_MajorityElementII Solutioon and Unit Test.

---
+++ LeetCode-C#/TopInterview150/Array_String/229_MajorityElementII.cs
@@ -0,0 +1,38 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// https://leetcode.com/problems/majority-element-ii/description/
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _229_MajorityElementII
+    {
+        public IList<int> MajorityElement(int[] nums)
+        {
+            int appeared = nums.Length / 3;
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            List<int> answer = new List<int>();
+
+            for (int idx = 0; idx < nums.Length; idx++)
+            {
+                if (dic.ContainsKey(nums[idx]))
+                {
+                    dic[nums[idx]]++;
+                }
+                else
+                {
+                    dic.Add(nums[idx], 1);
+                }
+            }
+
+            foreach (var elem in dic)
+            {
+                if (elem.Value > appeared)
+                {
+                    answer.Add(elem.Key);
+                }
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/229_MajorityElementII_Test.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _229_MajorityElementII_Test
+    {
+        private readonly _229_MajorityElementII _algorithm;
+
+        public _229_MajorityElementII_Test()
+        {
+            _algorithm = new _229_MajorityElementII();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 3, 2, 3 }, new int[] { 3 })]
+        [InlineData(new int[] { 1 }, new int[] { 1 })]
+        [InlineData(new int[] { 1, 2 }, new int[] { 1, 2 })]
+        public void MajorityElement_Test(int[] nums, int[] expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = _algorithm.MajorityElement(nums);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
