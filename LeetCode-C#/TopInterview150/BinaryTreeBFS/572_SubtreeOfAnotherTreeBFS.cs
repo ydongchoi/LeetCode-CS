dce3f5f17ad5e95571f5637c40a08be1c655e561
ydongchoi <105weapon@gmail.com>
2024-09-18 12:29:50 -0700

    [feature] Added 572_SubtreeOfAnother Tree BFS Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/BinaryTreeBFS/572_SubtreeOfAnotherTreeBFS.cs
@@ -0,0 +1,56 @@
+﻿
+// Time Complexity : O(NM), Spacious Complexity : O(N + M)
+// Link : https://leetcode.com/problems/subtree-of-another-tree/
+
+namespace LeetCode_C_.TopInterview150.BinaryTree
+{
+    public class _572_SubtreeOfAnotherTree_BFS
+    {
+        public bool IsSubtree(TreeNode root, TreeNode subRoot)
+        {
+            Queue<TreeNode> rQueue = new Queue<TreeNode>();
+            rQueue.Enqueue(root);
+
+            while (rQueue.Count != 0)
+            {
+                TreeNode rCur = rQueue.Dequeue();
+
+                if (rCur.val == subRoot.val)
+                {
+                    if (IsSameTree(rCur, subRoot)) return true;
+                }
+
+                if (rCur.left is not null) rQueue.Enqueue(rCur.left);
+                if (rCur.right is not null) rQueue.Enqueue(rCur.right);
+            }
+
+            return false;
+        }
+
+        public bool IsSameTree(TreeNode root, TreeNode subRoot)
+        {
+            Queue<TreeNode> rQueue = new Queue<TreeNode>();
+            Queue<TreeNode> sQueue = new Queue<TreeNode>();
+            rQueue.Enqueue(root); sQueue.Enqueue(subRoot);
+
+            while (rQueue.Count != 0 && sQueue.Count != 0)
+            {
+                TreeNode rCur = rQueue.Dequeue(); TreeNode sCur = sQueue.Dequeue();
+
+                if (rCur is not null && sCur is null) return false;
+                if (rCur is null && sCur is not null) return false;
+                if (rCur is null && sCur is null) continue;
+                if (rCur.val != sCur.val) return false;
+
+                rQueue.Enqueue(rCur.left);
+                rQueue.Enqueue(rCur.right);
+
+                sQueue.Enqueue(sCur.left);
+                sQueue.Enqueue(sCur.right);
+            }
+
+            if (rQueue.Count != sQueue.Count) return false;
+            else return true;
+        }
+    }
+}
---
+++ Tests/TopInterview150/BinaryTreeBFS/572_SubtreeOfAnotherTreeBFS_Test.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode_C_.TopInterview150.BinaryTree;
+
+namespace Tests.TopInterview150.BinaryTreeBFS
+{
+    public class _572_SubtreeOfAnotherTreeBFS_Test
+    {
+        private readonly _572_SubtreeOfAnotherTree_BFS _algorithm;
+
+        public _572_SubtreeOfAnotherTreeBFS_Test()
+        {
+            _algorithm = new _572_SubtreeOfAnotherTree_BFS();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void IsSubtree_Input_Returns(List<int?> trees, List<int?> subTrees, bool expectedResult)
+        {
+            // Assign
+            TreeNode root = TreeNode.GetTreeNode(trees);
+            TreeNode subRoot = TreeNode.GetTreeNode(subTrees);
+
+            // Act
+            var result = _algorithm.IsSubtree(root, subRoot);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { new List<int?> { 3, 4, 5, 1, 2 }, new List<int?> { 4, 1, 2 }, true };
+        }
+    }
+}
+
