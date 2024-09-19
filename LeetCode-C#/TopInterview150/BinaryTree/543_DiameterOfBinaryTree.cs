c263de5ce3b67ec7b5555469d133872aa7f19adb
ydongchoi <105weapon@gmail.com>
2024-09-19 15:07:10 -0700

    [feature] 543_DiameterOfBinaryTree Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/BinaryTree/543_DiameterOfBinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/diameter-of-binary-tree/
+
+namespace LeetCode_C_.TopInterview150.BinaryTree
+{
+    public class _543_DiameterOfBinaryTree
+    {
+        private int _answer;
+
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            _answer = 0;
+            GetMaxDepth(root);
+
+            return _answer;
+        }
+
+        public int GetMaxDepth(TreeNode cur)
+        {
+            if (cur is null)
+            {
+                return -1;
+            }
+
+            int left = GetMaxDepth(cur.left) + 1;
+            int right = GetMaxDepth(cur.right) + 1;
+
+            int merge = left + right;
+
+            _answer = _answer >= merge ? _answer : merge;
+
+            return left > right ? left : right;
+        }
+    }
+}
---
+++ Tests/TopInterview150/BinaryTree/543_DiameterOfBinaryTree_Test.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode_C_.TopInterview150.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.BinaryTree
+{
+    public class _543_DiameterOfBinaryTree_Test
+    {
+        private readonly _543_DiameterOfBinaryTree _algirthm;
+
+        public _543_DiameterOfBinaryTree_Test()
+        {
+            _algirthm = new _543_DiameterOfBinaryTree();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void DiameterOfBinaryTree_Input_Returns(List<int?> trees, int expectedResult)
+        {
+            // Assign
+            TreeNode root = TreeNode.GetTreeNode(trees);
+
+            // Act
+            var result = _algirthm.DiameterOfBinaryTree(root);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { new List<int?> { 1, 2, 3, 4, 5 }, 3 };
+            yield return new object[] { new List<int?> { 1, 2 }, 1 };
+        }
+    }
+}
