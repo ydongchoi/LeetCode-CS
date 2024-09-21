39100be6a2c8335498aec561f3d6731e527d17bd
ydongchoi <105weapon@gmail.com>
2024-09-20 19:36:24 -0700

    [feature] Added 230_Kth_SmallesElementBST Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/BinaryTree/230_Kth SmallestElementInaBST.cs
@@ -0,0 +1,29 @@
+﻿
+// Time Complexity : O(NLogN), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+namespace LeetCode_C_.TopInterview150.BinaryTree
+{
+    public class _230_Kth_SmallestElementInaBST
+    {
+        public int KthSmallest(TreeNode root, int k)
+        {
+            List<int> nodes = new List<int>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                TreeNode cur = queue.Dequeue();
+                nodes.Add(cur.val);
+
+                if (cur.left is not null) queue.Enqueue(cur.left);
+                if (cur.right is not null) queue.Enqueue(cur.right);
+            }
+
+            nodes.Sort();
+
+            return nodes[k - 1];
+        }
+    }
+}
---
+++ Tests/TopInterview150/BinaryTree/230_Kth SmallestElementInaBST.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.BinaryTree
+{
+    public class _230_Kth_SmallestElementInaBST
+    {
+        public int KthSmallest(TreeNode root, int k)
+        {
+            List<int> nodes = new List<int>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                TreeNode cur = queue.Dequeue();
+                nodes.Add(cur.val);
+
+                if (cur.left is not null) queue.Enqueue(cur.left);
+                if (cur.right is not null) queue.Enqueue(cur.right);
+            }
+
+            nodes.Sort();
+
+            return nodes[k - 1];
+        }
+    }
+}
