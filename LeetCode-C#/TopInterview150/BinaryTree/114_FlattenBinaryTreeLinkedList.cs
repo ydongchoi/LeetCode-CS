2d599090bdb5243bd2191462ba982579ed9d104b
ydongchoi <105weapon@gmail.com>
2024-09-21 19:51:07 -0700

    [feature] Added 114_FlattenBinaryTreeLinkedList Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/BinaryTree/114_FlattenBinaryTreeLinkedList.cs
@@ -0,0 +1,49 @@
+﻿
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+
+namespace LeetCode_C_.TopInterview150.BinaryTree
+{
+    public  class _114_FlattenBinaryTreeLinkedList
+    {
+        private List<int> copied;
+
+        public void Flatten(TreeNode root)
+        {
+            if (root is null) return;
+
+            copied = new List<int>();
+
+            PreorderTraversal(root);
+
+            TreeNode prev = root;
+            TreeNode cur = root;
+
+            foreach (var elem in copied)
+            {
+                if (cur is not null)
+                {
+                    cur.val = elem;
+                }
+                else
+                {
+                    cur = new TreeNode(elem);
+                    prev.right = cur;
+                }
+
+                prev = cur;
+                cur.left = null;
+                cur = cur.right;
+            }
+        }
+
+        public void PreorderTraversal(TreeNode cur)
+        {
+            if (cur is null) return;
+
+            copied.Add(cur.val);
+            PreorderTraversal(cur.left);
+            PreorderTraversal(cur.right);
+        }
+    }
+}
---
+++ Tests/TopInterview150/BinaryTree/114_FlattenBinaryTreeLinkedList_Test.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode_C_.TopInterview150.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TopInterview150.BinaryTree
+{
+    public class _114_FlattenBinaryTreeLinkedList_Test
+    {
+        private readonly _114_FlattenBinaryTreeLinkedList _algorithm;
+
+        public _114_FlattenBinaryTreeLinkedList_Test()
+        {
+            _algorithm = new _114_FlattenBinaryTreeLinkedList();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Flatten_Input_Returns(List<int?> trees, List<int?> expectedResult)
+        {
+            // Arrange
+            TreeNode root = TreeNode.GetTreeNode(trees);
+
+            // Act
+            _algorithm.Flatten(root);
+
+            // Assert
+            TreeNode cur = root;
+            for(int idx  = 0; idx < expectedResult.Count; idx++)
+            {
+                Assert.Equal(cur.val, expectedResult[idx++]);
+                Assert.Equal(cur.left, null);
+
+                cur = cur.right;
+            }
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { new List<int?> { 1, 2, 5, 3, 4, null, 6 }, new List<int?> {1, null, 2, null, 3, null, 4, null, 5, null, 6} };
+        }
+    }
+}
