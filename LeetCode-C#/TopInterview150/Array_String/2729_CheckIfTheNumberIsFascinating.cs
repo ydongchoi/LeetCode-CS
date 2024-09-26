26a4a12ea6185543ae813278d0780014ef917fa7
ydongchoi <105weapon@gmail.com>
2024-09-26 14:06:30 -0700

    [feature] Created 2729_CheckIfTheNumberIsFascinating Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/Array_String/2729_CheckIfTheNumberIsFascinating.cs
@@ -0,0 +1,35 @@
+﻿
+// Time Complexity : O(1), Spacious Complexity : O(1)
+// Link : https://leetcode.com/problems/check-if-the-number-is-fascinating/description/
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _2729_CheckIfTheNumberIsFascinating
+    {
+        public bool IsFascinating(int n)
+        {
+            string first = n.ToString();
+            string second = (2 * n).ToString();
+            string third = (3 * n).ToString();
+
+            string total = first + second + third;
+            if (total.Length > 9) return false;
+
+            int[] numLen = new int[10];
+            for (int idx = 0; idx < total.Length; ++idx)
+            {
+                int number = total[idx] - '0';
+                if (number == 0) return false;
+
+                numLen[number]++;
+            }
+
+            for (int idx = 1; idx <= 9; ++idx)
+            {
+                if (numLen[idx] != 1) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/2729_CheckIfTheNumberIsFascinating_Test.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _2729_CheckIfTheNumberIsFascinating_Test
+    {
+        private readonly _2729_CheckIfTheNumberIsFascinating _algorithm;
+
+        public _2729_CheckIfTheNumberIsFascinating_Test()
+        {
+            _algorithm = new _2729_CheckIfTheNumberIsFascinating();
+        }
+
+        [Theory]
+        [InlineData(192, true)]
+        [InlineData(100, false)]
+        public void IsFascinating_Input_Returns(int input, bool expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var result = _algorithm.IsFascinating(input);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
