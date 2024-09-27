8ba12c96e8a1ed3961fae87dc792b3731112ee2f
ydongchoi <105weapon@gmail.com>
2024-09-27 11:55:41 -0700

    [feature] Created 139_WordBreak Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/DynamicProgramming/139_WordBreak.cs
@@ -0,0 +1,34 @@
+﻿
+// Time Complexity : O(2^N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/word-break/description/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150.DynamicProgramming
+{
+    public class _139_WordBreak
+    {
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            Dictionary<string, bool> memo = new Dictionary<string, bool>();
+            HashSet<string> wordSet = new HashSet<string>(wordDict);
+
+            return Dfs(s, wordSet, memo);
+        }
+
+        private bool Dfs(string s, HashSet<string> wordSet, Dictionary<string, bool> memo)
+        {
+            if (memo.ContainsKey(s)) return memo[s];
+            if (wordSet.Contains(s)) return true;
+            for (int i = 1; i < s.Length; i++)
+            {
+                string prefix = s.Substring(0, i);
+                if (wordSet.Contains(prefix) && Dfs(s.Substring(i), wordSet, memo))
+                {
+                    memo[s] = true;
+                    return true;
+                }
+            }
+            memo[s] = false;
+            return false;
+        }
+    }
+}
---
+++ Tests/TopInterview150/DynamicProgramming/139_WordBreak_Test.cs
@@ -0,0 +1,30 @@
+﻿
+using LeetCode_C_.TopInterview150.DynamicProgramming;
+
+namespace Tests.TopInterview150.DynamicProgramming
+{
+    public class _139_WordBreak_Test
+    {
+        private readonly _139_WordBreak _algorithm;
+
+        public _139_WordBreak_Test()
+        {
+            _algorithm = new _139_WordBreak();
+        }
+
+        [Theory]
+        [InlineData("leetcode", new string[] {"leet", "code"}, true)]
+        [InlineData("applepenapple", new string[] {"apple", "pen"}, true)]
+        [InlineData("catsandog", new string[] {"cats", "dog", "sand", "and", "cat"}, false)]
+        public void WordBreak_Input_Returns(string input1, string[] input2, bool expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var result = _algorithm.WordBreak(input1, input2);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
