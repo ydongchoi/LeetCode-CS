3288606db1be6a30d6d3f6344e58642c863c52b7
ydongchoi <105weapon@gmail.com>
2024-10-04 12:21:04 -0700

    [feature] Added 140_WordBreakII Solution and Unit test.

---
+++ LeetCode-C#/TopInterview150/BackTracking/140_WordBreakII.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+// Time Complexity : O(2^N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/word-break-ii/description/
+
+namespace LeetCode_C_.TopInterview150.BackTracking
+{
+    public class _140_WordBreakII
+    {
+        private List<string> _answer;
+        private HashSet<string> _dic;
+
+        public IList<string> WordBreak(string s, IList<string> wordDict)
+        {
+            _answer = new List<string>();
+            _dic = new HashSet<string>();
+            foreach (var elem in wordDict) _dic.Add(elem);
+
+            BackTracking(new StringBuilder(s), new StringBuilder());
+
+            return _answer;
+        }
+
+        public void BackTracking(StringBuilder sb, StringBuilder sentence)
+        {
+            if (sb.Length == 0)
+            {
+                _answer.Add(sentence.ToString(0, sentence.Length - 1));
+                return;
+            }
+
+            for (int idx = 0; idx < sb.Length; idx++)
+            {
+                var word = sb.ToString(0, idx + 1);
+                if (!_dic.Contains(word)) continue;
+
+                sentence.Append(word + " ");
+                BackTracking(new StringBuilder(sb.ToString(idx + 1, sb.Length - word.Length)), sentence);
+                sentence.Remove(sentence.Length - (word.Length + 1), word.Length + 1);
+            }
+        }
+    }
+}
---
+++ Tests/TopInterview150/BackTracking/140_WordBreakII_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150.BackTracking;
+using System.Collections.Immutable;
+
+namespace Tests.TopInterview150.BackTracking
+{
+    public class _140_WordBreakII_Test
+    {
+        private readonly _140_WordBreakII _algorithm;
+
+        public _140_WordBreakII_Test()
+        {
+            _algorithm = new _140_WordBreakII();
+        }
+
+        [Theory]
+        [InlineData("catsanddog", new string[] { "cat", "cats", "and", "sand", "dog" }, new string[] { "cats and dog", "cat sand dog" })]
+        [InlineData("pineapplepenapple", new string[] { "apple", "pen", "applepen", "pine", "pineapple" }, new string[] { "pine apple pen apple", "pineapple pen apple", "pine applepen apple" })]
+        [InlineData("catsandog", new string[] { "cats", "dog", "sand", "and", "cat" }, new string[] { })]
+        public void WordBreakII_Test(string s, string[] wordDict, string[] expected)
+        {
+            // Act
+            var result = _algorithm.WordBreak(s, wordDict);
+
+            // Assert
+            Assert.Equal(expected.Length, result.Count);
+            for(int i = 0; i < expected.Length; i++)
+            {
+                Assert.Contains(expected[i], result);
+            }
+        }
+    }
+}
