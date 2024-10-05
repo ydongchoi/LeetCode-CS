3b4949b1907a7d1e4ef0afe7602427a140ec3e92
ydongchoi <105weapon@gmail.com>
2024-10-05 14:52:45 -0700

    feaat : Added 917_ReverseOnlyLetters Solution using Data Structrue and indirect traversal and Unit test.

---
+++ LeetCode-C#/TopInterview150/Array_String/917_ReverseOnlyLetters.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+// Time Complexity : O(N), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/longest-common-prefix/?envType=study-plan-v2&envId=top-interview-150
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _917_ReverseOnlyLetters
+    {
+        public string ReverseOnlyLetters(string s)
+        {
+            StringBuilder sb = new StringBuilder(s);
+            List<char> memo = new List<char>();
+
+            for (int idx = s.Length - 1; idx >= 0; idx--)
+            {
+                if (isAlphabet(s[idx]))
+                {
+                    memo.Add(s[idx]);
+                }
+            }
+
+            int start = 0;
+            for (int idx = 0; idx < s.Length; ++idx)
+            {
+                if (isAlphabet(s[idx]))
+                {
+                    sb[idx] = memo[start++];
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private bool isAlphabet(char elem)
+        {
+            return 'a' <= elem && elem <= 'z' || 'A' <= elem && elem <= 'Z';
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/917_ReverseOnlyLetters_Test.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+namespace Tests.TopInterview150.Array_String
+{
+    public class _917_ReverseOnlyLetters_Test
+    {
+        private readonly _917_ReverseOnlyLetters _algorithm;
+
+        public _917_ReverseOnlyLetters_Test()
+        {
+            _algorithm = new _917_ReverseOnlyLetters();
+        }
+
+        [Theory]
+        [InlineData("ab-cd", "dc-ba")]
+        [InlineData("a-bC-dEf-ghIj", "j-Ih-gfE-dCba")]
+        [InlineData("Test1ng-Leet=code-Q!", "Qedo1ct-eeLg=ntse-T!")]
+        public void ReverseOnlyLetters_Input_Returns(string s, string expected)
+        {
+            //Act
+            var result = _algorithm.ReverseOnlyLetters(s);
+            
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
