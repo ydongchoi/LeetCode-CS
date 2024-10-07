dee4c6c1e1ca519767f4b6adc77b2c87308c3aa7
ydongchoi <105weapon@gmail.com>
2024-10-07 13:11:05 -0700

    feat : Added 322_CoingChange Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/DynamicProgramming/322_CoinChange.cs
@@ -0,0 +1,49 @@
+﻿
+// Time Complexity : O(CN), Spacious Complexity : O(N)
+// Link : https://leetcode.com/problems/coin-change/description/
+
+namespace LeetCode_C_.TopInterview150.DynamicProgramming
+{
+    public class _322_CoinChange
+    {
+        private int _answer;
+        private int[] _memo;
+
+        public int CoinChange(int[] coins, int amount)
+        {
+            _answer = 10001;
+            _memo = new int[10001];
+
+            DP(0, 0, coins, amount);
+
+            if (_answer == 10001) return -1;
+            return _answer;
+        }
+
+        private void DP(int cnt, int curAmount, int[] coins, int amount)
+        {
+            if (curAmount == amount)
+            {
+                _answer = _answer > cnt ? cnt : _answer;
+                return;
+            }
+            if (curAmount > amount)
+            {
+                return;
+            }
+
+            for (int idx = coins.Length - 1; idx >= 0; idx--)
+            {
+                if (coins[idx] > 10000) continue;
+
+                int nxtAmount = curAmount + coins[idx];
+                int nxtCnt = cnt + 1;
+
+                if (nxtCnt >= _answer || nxtAmount > amount || (_memo[nxtAmount] != 0 && _memo[nxtAmount] <= nxtCnt)) continue;
+
+                _memo[nxtAmount] = nxtCnt;
+                DP(nxtCnt, nxtAmount, coins, amount);
+            }
+        }
+    }
+}
---
+++ Tests/TopInterview150/DynamicProgramming/322_CoinChange_Test.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode_C_.TopInterview150.DynamicProgramming;
+
+namespace Tests.TopInterview150.DynamicProgramming
+{
+    public class _322_CoinChange_Test
+    {
+        private readonly _322_CoinChange _algorithm;
+
+        public _322_CoinChange_Test()
+        {
+            _algorithm = new _322_CoinChange();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 5 }, 11, 3)]
+        [InlineData(new int[] { 2 }, 3, -1)]
+        [InlineData(new int[] { 1 }, 0, 0)]
+        [InlineData(new int[] { 1 }, 1, 1)]
+        public void CoinChange_Input_Returns(int[] coins, int amount, int expected)
+        {
+            // Act
+            var result = _algorithm.CoinChange(coins, amount);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
