c79a35f3edd4db2f765b95f445fe3d346d353371
ydongchoi <105weapon@gmail.com>
2024-10-08 11:35:48 -0700

    feat : 62_UniquePaths Solution of BFS with Memo and Unit Test.

---
+++ LeetCode-C#/TopInterview150/DynamicProgramming/62_UniquePaths.cs
@@ -0,0 +1,43 @@
+﻿
+// Time Complexity : O(MN), Spacious Complexity : O(MN)
+// Link : https://leetcode.com/problems/unique-paths/description/
+
+namespace LeetCode_C_.TopInterview150.DynamicProgramming
+{
+    public class _62_UniquePaths
+    {
+        public int UniquePaths(int m, int n)
+        {
+            int[,] memo = new int[m, n];
+            bool[,] visited = new bool[m, n];
+            int[] nxtRow = [1, 0];
+            int[] nxtCol = [0, 1];
+
+            Queue<Tuple<int, int>> queue = new Queue<Tuple<int, int>>();
+            queue.Enqueue(Tuple.Create(0, 0));
+            memo[0, 0] = 1;
+
+            while (queue.Count != 0)
+            {
+                var cur = queue.Dequeue();
+                int curX = cur.Item1; int curY = cur.Item2;
+
+                if (visited[curX, curY]) continue;
+                visited[curX, curY] = true;
+
+                for (int idx = 0; idx < 2; idx++)
+                {
+                    int nxtX = curX + nxtRow[idx];
+                    int nxtY = curY + nxtCol[idx];
+
+                    if (nxtX < 0 || nxtX >= m || nxtY < 0 || nxtY >= n) continue;
+
+                    memo[nxtX, nxtY] = memo[nxtX, nxtY] + memo[curX, curY];
+                    queue.Enqueue(Tuple.Create(nxtX, nxtY));
+                }
+            }
+
+            return memo[m - 1, n - 1];
+        }
+    }
+}
---
+++ Tests/TopInterview150/DynamicProgramming/62_UniquePaths_Test.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode_C_.TopInterview150.DynamicProgramming;
+
+namespace Tests.TopInterview150.DynamicProgramming
+{
+    public class _62_UniquePaths_Test
+    {
+        private readonly _62_UniquePaths _algorithm;
+
+        public _62_UniquePaths_Test()
+        {
+            _algorithm = new _62_UniquePaths();
+        }
+
+        [Theory]
+        [InlineData(3, 7, 28)]
+        [InlineData(3, 2, 3)]
+        [InlineData(7, 3, 28)]
+        public void UniquePaths_Input_Returns(int m, int n, int expected)
+        {
+            // Act
+            var result = _algorithm.UniquePaths(m, n);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
