84289bf2af63c3d4680740c98c91b8bb2419a4ed
ydongchoi <105weapon@gmail.com>
2024-10-09 15:39:58 -0700

    feat : Added 5_LongestPalindromicSubstring Solution using 2Dimension DP and Unit Test.

---
+++ LeetCode-C#/TopInterview150/DynamicProgramming/5_LongestPalindromicSubstring.cs
@@ -0,0 +1,47 @@
+﻿
+// Time Complexity: O(n^2), Space Complexity: O(n^2)
+// Link: https://leetcode.com/problems/longest-palindromic-substring/description/
+
+namespace LeetCode_C_.TopInterview150.DynamicProgramming
+{
+    public class _5_LongestPalindromicSubstring
+    {
+        public string LongestPalindrome(string s)
+        {
+            // 2D - DP
+            int sLen = s.Length;
+            bool[,] memo = new bool[sLen, sLen];
+            int allStart = 0; int maxLen = 1;
+
+            // Initialization when Length is 1 or 2
+            for (int idx = 0; idx < sLen; ++idx)
+            {
+                memo[idx, idx] = true;
+
+                if (idx != sLen - 1 && s[idx] == s[idx + 1])
+                {
+                    memo[idx, idx + 1] = true;
+                    allStart = idx; maxLen = 2;
+                }
+            }
+
+            // Another Palindrome
+            for (int len = 2; len < sLen; ++len)
+            {
+                for (int start = 0; start < sLen - len; ++start)
+                {
+                    int end = start + len;
+
+                    if (s[start] == s[end] && memo[start + 1, end - 1])
+                    {
+                        memo[start, end] = true;
+                        allStart = start;
+                        maxLen = end - start + 1;
+                    }
+                }
+            }
+
+            return s.Substring(allStart, maxLen);
+        }
+    }
+}
---
+++ Tests/TopInterview150/DynamicProgramming/5_LongestPalindromicSubstring_Test.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode_C_.TopInterview150.DynamicProgramming;
+
+namespace Tests.TopInterview150.DynamicProgramming
+{
+    public class _5_LongestPalindromicSubstring_Test
+    {
+        private readonly _5_LongestPalindromicSubstring _algorithm;
+
+        public _5_LongestPalindromicSubstring_Test()
+        {
+            _algorithm = new _5_LongestPalindromicSubstring();
+        }
+
+        [Theory]
+        [InlineData("babad", "aba")]
+        [InlineData("cbbd", "bb")]
+        [InlineData("a", "a")]
+        public void LongestPalindrome_Input_Returns(string s, string expected)
+        {
+            // Act
+            var result = _algorithm.LongestPalindrome(s);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
