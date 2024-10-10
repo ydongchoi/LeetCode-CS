b03b17d447f40983b0ce82e01d0dad8a3edc3c76
ydongchoi <105weapon@gmail.com>
2024-10-10 13:16:05 -0700

    feat : 338_CountingBits Solution with Dynamic Programming and Unit Test.

---
+++ LeetCode-C#/TopInterview150/DynamicProgramming/338_CountingBits.cs
@@ -0,0 +1,27 @@
+﻿
+// Time: O(n), Space: O(n)
+// Link: https://leetcode.com/problems/counting-bits/
+
+namespace LeetCode_C_.TopInterview150.DynamicProgramming
+{
+    public class _338_CountingBits
+    {
+        public int[] CountBits(int n)
+        {
+            if (n == 0) return new int[] { 0 };
+
+            int[] memo = new int[n + 1];
+            memo[0] = 0; memo[1] = 1;
+
+            for (int num = 2; num <= n; ++num)
+            {
+                int division = num / 2;
+                int remain = num % 2;
+
+                memo[num] = memo[division] + memo[remain];
+            }
+
+            return memo;
+        }
+    }
+}
---
+++ Tests/TopInterview150/DynamicProgramming/338_CountingBits_Test.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode_C_.TopInterview150.DynamicProgramming;
+
+namespace Tests.TopInterview150.DynamicProgramming
+{
+    public class _338_CountingBits_Test
+    {
+        private readonly _338_CountingBits _algorithm;
+
+        public _338_CountingBits_Test()
+        {
+            _algorithm = new _338_CountingBits();
+        }
+
+        [Theory]
+        [InlineData(2, new int[] {0,1,1})]
+        [InlineData(5, new int[] {0,1,1,2,1,2 })]
+        public void CountBits_Input_Returns(int n, int[] expected)
+        {
+            // Act
+            var result = _algorithm.CountBits(n);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
