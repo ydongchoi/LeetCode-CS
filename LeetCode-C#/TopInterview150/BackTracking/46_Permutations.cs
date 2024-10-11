aa40f8d758ee5465ec33d16f95bf69cbcf14c0f9
ydongchoi <105weapon@gmail.com>
2024-10-11 15:58:56 -0700

    feat : Added 46_Permutations Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/BackTracking/46_Permutations.cs
@@ -0,0 +1,39 @@
+﻿
+// Time Complexity: O(n!), Space Complexity: O(N^2)
+// Linke: https://leetcode.com/problems/permutations/description/?envType=problem-list-v2&envId=backtracking
+
+namespace LeetCode_C_.TopInterview150.BackTracking
+{
+    public class _46_Permutations
+    {
+        private bool[] _isVisited;
+        private List<IList<int>> _answer;
+
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            _isVisited = new bool[nums.Length];
+            _answer = new List<IList<int>>();
+
+            BackTracking(new List<int>(), 0, nums);
+
+            return _answer;
+        }
+
+        private void BackTracking(List<int> one, int choice, int[] nums)
+        {
+            if (choice == nums.Length)
+            {
+                _answer.Add(new List<int>(one));
+            }
+
+            for (int idx = 0; idx < nums.Length; ++idx)
+            {
+                if (_isVisited[idx]) continue;
+
+                _isVisited[idx] = true; one.Add(nums[idx]);
+                BackTracking(one, choice + 1, nums);
+                _isVisited[idx] = false; one.Remove(nums[idx]);
+            }
+        }
+    }
+}
---
+++ Tests/TopInterview150/BackTracking/46_Permutations_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_C_.TopInterview150.BackTracking;
+
+namespace Tests.TopInterview150.BackTracking
+{
+    public class _46_Permutations_Test
+    {
+        private readonly _46_Permutations _algorithm;
+
+        public _46_Permutations_Test()
+        {
+            _algorithm = new _46_Permutations();
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3 }, new[] { 1, 2, 3 }, new[] { 1, 3, 2 }, new[] { 2, 1, 3 }, new[] { 2, 3, 1 }, new[] { 3, 1, 2 }, new[] { 3, 2, 1 })]
+        [InlineData(new[] { 0, 1 }, new[] { 0, 1 }, new[] { 1, 0 })]
+        [InlineData(new[] { 1 }, new[] { 1 })]
+        public void Permute_Input_Returns(int[] nums, params int[][] expected)
+        {
+            // Act
+            var result = _algorithm.Permute(nums);
+
+            // Assert
+            Assert.Equal(expected.Length, result.Count);
+
+            for (var i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], result[i]);
+            }
+        }
+    }
+}
