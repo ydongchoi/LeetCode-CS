762ab9ffa7c4d9445b564cac9705203ef3dbef79
ydongchoi <105weapon@gmail.com>
2024-10-13 15:23:03 -0700

    feat : Added 235_LowestCommonAncestorOfBinarySearchTree Solution traversing Preorder and Unit Test.

---
+++ LeetCode-C#/TopInterview150/BinaryTree/235_LowestCommonAncestorOfABinarySearchTree.cs
@@ -0,0 +1,47 @@
+﻿
+// Time Complexity : O(N), Space Complexity : O(N)
+// Link : https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+namespace LeetCode_C_.TopInterview150.BinaryTree
+{
+    public class _235_LowestCommonAncestorOfABinarySearchTree
+    {
+        private List<TreeNode> _pList;
+        private List<TreeNode> _qList;
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            int pVal = p.val; int qVal = q.val;
+            TreeNode answer = null;
+
+            _pList = Preorder(root, pVal, new List<TreeNode>());
+            _qList = Preorder(root, qVal, new List<TreeNode>());
+
+            int pCount = _pList.Count(); int qCount = _qList.Count();
+            int cnt = pCount > qCount ? qCount : pCount;
+
+            for (int idx = 0; idx < cnt; ++idx)
+            {
+                if (_pList[idx].val == _qList[idx].val)
+                {
+                    answer = _pList[idx];
+                }
+            }
+
+            return answer;
+        }
+
+        private List<TreeNode> Preorder(TreeNode root, int val, List<TreeNode> path)
+        {
+            path.Add(root);
+
+            if (val == root.val)
+            {
+                return path;
+            }
+
+            if (val < root.val) return Preorder(root.left, val, path);
+            else return Preorder(root.right, val, path);
+        }
+    }
+}
---
+++ Tests/TopInterview150/BinaryTree/235_LowestCommonAncestorOfABinarySearchTree_Test.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode_C_.TopInterview150.BinaryTree;
+
+namespace Tests.TopInterview150.BinaryTree
+{
+    public class _235_LowestCommonAncestorOfABinarySearchTree_Test
+    {
+        private readonly _235_LowestCommonAncestorOfABinarySearchTree _algorithm;
+
+        public _235_LowestCommonAncestorOfABinarySearchTree_Test()
+        {
+            _algorithm = new _235_LowestCommonAncestorOfABinarySearchTree();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void LowestCommonAncestor_Input_Returns(List<int?> trees, int p, int q, int expected)
+        {
+            // Arrange
+            TreeNode root = TreeNode.GetTreeNode(trees);
+
+            // Act
+            var result = _algorithm.LowestCommonAncestor(root, new TreeNode(p), new TreeNode(q));
+
+            // Assert
+            Assert.Equal(expected, result.val);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { new List<int?> { 6, 2, 8, 0, 4, 7, 9, null, null, 3, 5 }, 2, 8, 6 };
+            yield return new object[] { new List<int?> { 6, 2, 8, 0, 4, 7, 9, null, null, 3, 5 }, 2, 4, 2 };
+        }
+    }
+}
