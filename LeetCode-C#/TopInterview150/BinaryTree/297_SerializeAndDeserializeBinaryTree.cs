4c9fe989e38ee5cf1d22259a70661aec2c16ed74
ydongchoi <105weapon@gmail.com>
2024-10-15 14:34:57 -0700

    feat : Added 297_SerializeAndDeserializeBinaryTree Solution doing level order traversal using Queue and Unit test.

---
+++ LeetCode-C#/TopInterview150/BinaryTree/297_SerializeAndDeserializeBinaryTree.cs
@@ -0,0 +1,83 @@
+﻿
+// Time Complexity: O(n), Spacing Complexity: O(n)
+// Link : https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+
+using System.Text;
+
+namespace LeetCode_C_.TopInterview150.BinaryTree
+{
+    public class _297_SerializeAndDeserializeBinaryTree
+    {
+        // Encodes a tree to a single string.
+        public string serialize(TreeNode root)
+        {
+            if (root is null) return "";
+
+            StringBuilder sb = new StringBuilder();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                TreeNode cur = queue.Dequeue();
+
+                if (cur is null)
+                {
+                    sb.Append(" _");
+                    continue;
+                }
+                sb.Append(cur.val.ToString() + "_");
+
+                queue.Enqueue(cur.left);
+                queue.Enqueue(cur.right);
+            }
+
+            return sb.ToString();
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(string data)
+        {
+            if (data.Length == 0) return null;
+
+            string[] numbers = data.Split("_");
+            int idx = 0;
+
+            TreeNode root = new TreeNode(Int32.Parse(numbers[idx++]));
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                TreeNode cur = queue.Dequeue();
+
+                if (cur is null) continue;
+
+                string left = numbers[idx++];
+                if (left.Equals(" "))
+                {
+                    cur.left = null;
+                }
+                else
+                {
+                    cur.left = new TreeNode(Int32.Parse(left));
+                }
+                queue.Enqueue(cur.left);
+
+                string right = numbers[idx++];
+                if (right.Equals(" "))
+                {
+                    cur.right = null;
+                }
+                else
+                {
+                    cur.right = new TreeNode(Int32.Parse(right));
+                }
+                queue.Enqueue(cur.right);
+            }
+
+            return root;
+        }
+
+    }
+}
---
+++ Tests/TopInterview150/BinaryTree/297_SerializeAndDeserializeBinaryTree_Test.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode_C_.TopInterview150.BinaryTree;
+
+namespace Tests.TopInterview150.BinaryTree
+{
+    public class _297_SerializeAndDeserializeBinaryTree_Test
+    {
+        private readonly _297_SerializeAndDeserializeBinaryTree _algorithm;
+
+        public _297_SerializeAndDeserializeBinaryTree_Test()
+        {
+            _algorithm = new _297_SerializeAndDeserializeBinaryTree();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test1(List<int?> trees, List<int?> expected)
+        {
+            // Assign
+            TreeNode root = TreeNode.GetTreeNode(trees);
+
+            // Act
+            string serialized = _algorithm.serialize(root);
+            TreeNode deserialized = _algorithm.deserialize(serialized);
+
+            // Assert
+            Assert.Equal(expected, TreeNode.GetValues(deserialized));
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { new List<int?> { 1, 2, 3, null, null, 4, 5 }, new List<int?> { 1, 2, 3, null, null, 4, 5 } };
+        }
+
+    }
+}
