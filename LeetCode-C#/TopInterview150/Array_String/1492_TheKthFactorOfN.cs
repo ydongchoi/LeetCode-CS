342976ac54b2c8df33cb311a14fce48673f98040
ydongchoi <105weapon@gmail.com>
2024-10-17 11:41:50 -0700

    feat : Added 1492_TheKthFactorOfN Solution and Unit Test.

---
+++ LeetCode-C#/TopInterview150/Array_String/1492_TheKthFactorOfN.cs
@@ -0,0 +1,27 @@
+﻿
+// Time complexity: O(n), Space complexity: O(1)
+// Link: https://leetcode.com/problems/the-kth-factor-of-n/?envType=study-plan-v2&envId=amazon-spring-23-high-frequency
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _1492_TheKthFactorOfN
+    {
+        public int KthFactor(int n, int k)
+        {
+            int cnt = 0;
+
+            for (int idx = 1; idx <= n / 2; ++idx)
+            {
+                if (n % idx == 0)
+                {
+                    cnt++;
+                    if (cnt == k) return idx;
+                }
+            }
+            cnt++;
+
+            if (cnt == k) return n;
+            return -1;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/1492_TheKthFactorOfN_Test.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _1492_TheKthFactorOfN_Test
+    {
+        private readonly _1492_TheKthFactorOfN _algorithm;
+
+        public _1492_TheKthFactorOfN_Test()
+        {
+            _algorithm = new _1492_TheKthFactorOfN();
+        }
+
+        [Theory]
+        [InlineData(12, 3, 3)]
+        [InlineData(7, 2, 7)]
+        [InlineData(4, 4, -1)]
+        public void KthFactor_Input_Retuns(int n, int k, int expected)
+        {
+            // Act
+            var result = _algorithm.KthFactor(n, k);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
