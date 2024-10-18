8f5eac3ebfbd05e6fe3b30953f52eb0dd0005c08
ydongchoi <105weapon@gmail.com>
2024-10-18 12:39:53 -0700

    feat : Added 2405_OptimalPartitionOfString Solution using Greedy and Unit Test.

---
+++ LeetCode-C#/TopInterview150/Array_String/2405_OptimalPartitionOfString.cs
@@ -0,0 +1,30 @@
+﻿
+// Time Complexity: O(n), Space Complexity: O(N)
+// Link : https://leetcode.com/problems/optimal-partition-of-string/description/?envType=study-plan-v2&envId=amazon-spring-23-high-frequency
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _2405_OptimalPartitionOfString
+    {
+        public int PartitionString(string s)
+        {
+            int answer = 1;
+            HashSet<char> set = new HashSet<char>();
+
+            for (int idx = 0; idx < s.Length; ++idx)
+            {
+                if (!set.Contains(s[idx]))
+                {
+                    set.Add(s[idx]);
+                }
+                else
+                {
+                    set.Clear(); set.Add(s[idx]);
+                    answer = answer + 1;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/2405_OptimalPartitionOfString_Test.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _2405_OptimalPartitionOfString_Test
+    {
+        private readonly _2405_OptimalPartitionOfString _algorithm;
+
+        public _2405_OptimalPartitionOfString_Test()
+        {
+            _algorithm = new _2405_OptimalPartitionOfString();
+        }
+
+        [Theory]
+        [InlineData("abacaba", 4)]
+        [InlineData("ssssss", 6)]
+        public void PartitionString_Input_Returns(string s, int expected)
+        {
+            // Act
+            var result = _algorithm.PartitionString(s);
+            
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
