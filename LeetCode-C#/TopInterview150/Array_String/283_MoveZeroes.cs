83b2f459ae50c9e33c2f8b98623212cb27e52757
ydongchoi <105weapon@gmail.com>
2024-10-31 20:35:25 -0700

    feat : Created 283_MoveZeroes Solution and Uni Test.

---
+++ LeetCode-C#/TopInterview150/Array_String/283_MoveZeroes.cs
@@ -0,0 +1,24 @@
+﻿
+// Time Complexity: O(n), Spacious Complexity: O(1)
+// Link :https://leetcode.com/problems/move-zeroes/description/?envType=problem-list-v2&envId=array&difficulty=EASY
+
+namespace LeetCode_C_.TopInterview150.Array_String
+{
+    public class _283_MoveZeroes
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int p = 0;
+            for (int idx = 0; idx < nums.Length; ++idx)
+            {
+                if (nums[idx] != 0)
+                {
+                    int tmp = nums[p];
+                    nums[p] = nums[idx];
+                    nums[idx] = tmp;
+                    p++;
+                }
+            }
+        }
+    }
+}
---
+++ Tests/TopInterview150/Array_String/283_MoveZeroes_Test.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode_C_.TopInterview150.Array_String;
+
+namespace Tests.TopInterview150.Array_String
+{
+    public class _283_MoveZeroes_Test
+    {
+        private readonly _283_MoveZeroes _algorithm;
+
+        public _283_MoveZeroes_Test()
+        {
+            _algorithm = new _283_MoveZeroes();
+        }
+
+        [Theory]
+        [InlineData(new[] { 0, 1, 0, 3, 12 }, new[] { 1, 3, 12, 0, 0 })]
+        [InlineData(new[] { 0 }, new[] { 0 })]
+        public void MoveZeroes_Test(int[] nums, int[] expected)
+        {
+            // Act
+            _algorithm.MoveZeroes(nums);
+            
+            // Assert
+            Assert.Equal(expected, nums);
+        }
+    }
+}
